a6edb5fdc9145d5517691f0c3b81101925024f06
Stefan Idler <sidler@mulchprod.de>
2017-06-14 21:30:59 +0200

    UPDATE: removed legacy folders and various plugins / parts only relevant for the portal

--- module_packageserver/system/config/module_packageserver_id.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*"******************************************************************************************************
-*   (c) 2007-2016 by Kajona, www.kajona.de                                                              *
-*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
-********************************************************************************************************/
-namespace Kajona\Packageserver\System\Config;
-
-define("_packageserver_module_id_",	140);
-
