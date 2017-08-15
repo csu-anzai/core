573d677dee4ec9d80ce582c55f214caaeb737e84
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-08-15 17:39:41 +0200

    UPDATE: module_flow | lang_flow_de > add missing properties

--- module_flow/lang/module_flow/lang_flow_de.php
+++ module_flow/lang/module_flow/lang_flow_de.php
@@ -66,5 +66,5 @@ $lang["form_next_status_description"]   = "Auf dieser Seite können Sie für das
 
 $lang["action_status_change_title"]   = "Rückmeldung";
 $lang["action_status_change_success"]   = "Statuswechsel erfolgreich durchgeführt.";
-$lang["action_status_change_redirect"]   = "Statuswechsel erfolgreich durchgeführt.";
+$lang["action_status_change_redirect"]   = "Das System benötigt eine Benutzereingabe um den Statusübergang durchzuführen.";
 
