e73a04f20c724583cdfe62b4a2d769c0d80835c4
Stefan Idler <sidler@mulchprod.de>
2017-08-25 15:27:01 +0200

    NEW: system | UserGroup.php -> membership assignments are now versionable

--- module_system/lang/module_user/lang_user_de.php
+++ module_system/lang/module_user/lang_user_de.php
@@ -164,3 +164,4 @@ $lang["userlist_filter"]                 = "Filter anwenden";
 $lang["usersource_kajona_name"]          = "Kajona intern";
 $lang["user_copy_info"]                  = "Die Gruppeneinstellungen werden von dem Benutzer {0} kopiert. Im nächsten Schritt ist es möglich diese weiter anzupassen.";
 $lang["user_last_pwchanges_info"]        = "Benutzer welche das Passwort zurückgesetzt haben:";
+$lang["change_type_group"]               = "Gruppe";
