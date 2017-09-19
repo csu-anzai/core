9da0e6d913546e6d623c714d5bf516f8e522cea4
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-09-19 10:57:01 +0200

    UPDATE: module_system | MessagingTest > move queue tests into message test

--- module_system/tests/MessagingTest.php
+++ module_system/tests/MessagingTest.php
@@ -2,19 +2,34 @@
 
 namespace Kajona\System\Tests;
 
+use Kajona\System\System\Database;
+use Kajona\System\System\Date;
 use Kajona\System\System\Messageproviders\MessageproviderExceptions;
 use Kajona\System\System\MessagingMessage;
 use Kajona\System\System\MessagingMessagehandler;
+use Kajona\System\System\MessagingQueue;
 use Kajona\System\System\SystemSetting;
 use Kajona\System\System\UserGroup;
+use Kajona\System\System\UserUser;
 
 class MessagingTest extends Testbase
 {
+    public function setUp()
+    {
+        parent::setUp();
 
+        $this->removeAllQueueEntries();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        $this->removeAllQueueEntries();
+    }
 
     public function testSendMessage()
     {
-
         $strText = generateSystemid() . " autotest";
         $strTitle = generateSystemid() . " title";
         $strIdentifier = generateSystemid() . " identifier";
@@ -38,15 +53,12 @@ class MessagingTest extends Testbase
             }
         }
 
-
         $this->assertTrue($bitFound);
         $this->flushDBCache();
     }
 
-
     public function testSendMessageObject()
     {
-
         $strText = generateSystemid() . " autotest";
         $strTitle = generateSystemid() . " title";
         $strIdentifier = generateSystemid() . " identifier";
@@ -90,6 +102,95 @@ class MessagingTest extends Testbase
         $this->flushDBCache();
     }
 
+    public function testSendMessageObjectWithSendDateFuture()
+    {
+        $strText = generateSystemid() . " autotest";
+        $strTitle = generateSystemid() . " title";
+        $strIdentifier = generateSystemid() . " identifier";
+        $strSender = generateSystemid();
+        $strReference = generateSystemid();
+        $objSendDate = new Date();
+        $objSendDate->setNextDay();
+
+        $objMessage = new MessagingMessage();
+        $objMessage->setStrTitle($strTitle);
+        $objMessage->setStrBody($strText);
+        $objMessage->setStrInternalIdentifier($strIdentifier);
+        $objMessage->setObjMessageProvider(new MessageproviderExceptions());
+        $objMessage->setStrSenderId($strSender);
+        $objMessage->setStrMessageRefId($strReference);
+
+        $objUser = UserUser::getAllUsersByName("user")[0];
+
+        $objMessageHandler = new MessagingMessagehandler();
+        $objMessageHandler->sendMessageObject($objMessage, $objUser, $objSendDate);
+
+        $arrQueue = MessagingQueue::getObjectListFiltered();
+        $this->assertEquals(1, count($arrQueue));
+
+        $arrMessages = MessagingMessage::getMessagesByIdentifier($strIdentifier);
+        $this->assertEquals(0, count($arrMessages));
+
+        /** @var MessagingQueue $objQueue */
+        $objQueue = $arrQueue[0];
+
+        // the sender sets the date to the beginning of the day
+        $objSendDate->setBeginningOfDay();
+
+        $this->assertInstanceOf(MessagingQueue::class, $objQueue);
+        $this->assertEquals($objUser->getSystemid(), $objQueue->getStrReceiver());
+        $this->assertEquals($objSendDate->getLongTimestamp(), $objQueue->getObjSendDate()->getLongTimestamp());
+
+        $objMessage = $objQueue->getMessage();
+
+        $this->assertInstanceOf(MessagingMessage::class, $objMessage);
+        $this->assertEquals($strTitle, $objMessage->getStrTitle());
+        $this->assertEquals($strText, $objMessage->getStrBody());
+        $this->assertEquals($strIdentifier, $objMessage->getStrInternalIdentifier());
+        $this->assertEquals(MessageproviderExceptions::class, $objMessage->getStrMessageProvider());
+        $this->assertEquals($strSender, $objMessage->getStrSenderId());
+        $this->assertEquals($strReference, $objMessage->getStrMessageRefId());
+    }
+
+    public function testSendMessageObjectWithSendDateNow()
+    {
+        $strText = generateSystemid() . " autotest";
+        $strTitle = generateSystemid() . " title";
+        $strIdentifier = generateSystemid() . " identifier";
+        $strSender = generateSystemid();
+        $strReference = generateSystemid();
+        $objSendDate = new Date();
+
+        $objMessage = new MessagingMessage();
+        $objMessage->setStrTitle($strTitle);
+        $objMessage->setStrBody($strText);
+        $objMessage->setStrInternalIdentifier($strIdentifier);
+        $objMessage->setObjMessageProvider(new MessageproviderExceptions());
+        $objMessage->setStrSenderId($strSender);
+        $objMessage->setStrMessageRefId($strReference);
+
+        $objUser = UserUser::getAllUsersByName("user")[0];
+
+        $objMessageHandler = new MessagingMessagehandler();
+        $objMessageHandler->sendMessageObject($objMessage, $objUser, $objSendDate);
+
+        $arrQueue = MessagingQueue::getObjectListFiltered();
+        $this->assertEquals(0, count($arrQueue));
+
+        $arrMessages = MessagingMessage::getMessagesByIdentifier($strIdentifier);
+        $this->assertEquals(1, count($arrMessages));
+
+        /** @var MessagingQueue $objQueue */
+        $objMessage = $arrMessages[0];
+
+        $this->assertInstanceOf(MessagingMessage::class, $objMessage);
+        $this->assertEquals($strTitle, $objMessage->getStrTitle());
+        $this->assertEquals($strText, $objMessage->getStrBody());
+        $this->assertEquals($strIdentifier, $objMessage->getStrInternalIdentifier());
+        $this->assertEquals(MessageproviderExceptions::class, $objMessage->getStrMessageProvider());
+        $this->assertEquals($strSender, $objMessage->getStrSenderId());
+        $this->assertEquals($strReference, $objMessage->getStrMessageRefId());
+    }
 
     public function testUnreadCount()
     {
@@ -123,11 +224,14 @@ class MessagingTest extends Testbase
             }
         }
 
-
         $this->assertTrue($bitFound);
         $this->flushDBCache();
     }
 
-
+    private function removeAllQueueEntries()
+    {
+        $strPrefix = _dbprefix_;
+        Database::getInstance()->_pQuery("DELETE FROM {$strPrefix}messages_queue WHERE 1=1", []);
+    }
 }
 
