630ee2e5c495017b33f26a4b398bd6c632c37b78
smeyer <stefan.meyer@artemeon.de>
2017-09-22 14:26:44 +0200

    NEW: system  | HierarchyValidatorBase.php -> fixed validation if object is new

--- module_system/system/validators/HierarchyValidatorBase.php
+++ module_system/system/validators/HierarchyValidatorBase.php
@@ -58,35 +58,82 @@ class HierarchyValidatorBase implements HierarchyValidatorInterface
      */
     public function validateHierarchy(Root $objObject, $strNewParentId)
     {
-        //1. Same parent id
-        if ($objObject->getStrPrevId() === $strNewParentId) {
-            return true;
-        }
+        $bitIsNew = !validateSystemid($objObject->getStrSystemid());
 
-        //2. Check if movable
-        if (!$this->isMovable($objObject)) {
-            return false;
+        if (!$bitIsNew) {//only check this if object already exists
+            //1. Same parent id
+            if ($objObject->getStrPrevId() === $strNewParentId) {
+                return true;
+            }
+
+            //2. Check if movable
+            if (!$this->isMovable($objObject)) {
+                return false;
+            }
         }
 
         $objNewObjectParent = Objectfactory::getInstance()->getObject($strNewParentId);
         if ($objNewObjectParent !== null) {
             //3. Check if given node is a valid child for the given new parent
-            $objValidatorParent = HierarchyValidatorFactory::newHierarchyValidator($objNewObjectParent);
-            $arrValidChildren = $objValidatorParent->getArrValidChildNodes($objNewObjectParent);
-            if (!in_array(get_class($objObject), $arrValidChildren)) {
+            if (!$this->isValidChildNodeForParent($objObject, $objNewObjectParent)) {
                 return false;
             }
 
-            //4. Check if one the parents of the given node has ParentPathCheckActive set
-            $objParentNode = $this->getNodeWithParentPathCheckActive($objObject);
-            if ($objParentNode !== null) {
-                $arrPathIds = $objParentNode->getPathArray("", SystemModule::getModuleIdByNr($objParentNode->getIntModuleNr()));
-                return in_array($strNewParentId, $arrPathIds);
+            //4. Check if the given node has a valid parent
+            if (!$this->isValidParentForChildNode($objObject, $objNewObjectParent)) {
+                return false;
             }
         }
 
         return true;
     }
+
+
+    /**
+     * Checks if the given node is a valid of the given parent.
+     *
+     * @param Root $objObject
+     * @param Root $objNewObjectParent
+     * @return bool
+     */
+    private function isValidChildNodeForParent(Root $objObject, Root $objNewObjectParent)
+    {
+        //Check if given node is a valid child for the given new parent
+        $objValidatorParent = HierarchyValidatorFactory::newHierarchyValidator($objNewObjectParent);
+        $arrValidChildren = $objValidatorParent->getArrValidChildNodes($objNewObjectParent);
+        if (!in_array(get_class($objObject), $arrValidChildren)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the current node has a valid parent node
+     *
+     * Determines if one of the parent nodes of the given node '$objObject' has isParentPathCheckActive set to true.
+     *  If this is not the case, everything is ok -> return true
+     *  If this is case it will checked, if the the new parent node '$objNewObjectParent' is somewhere within that path
+     *
+     * @param Root $objObject
+     * @param Root $objNewObjectParent
+     * @return bool
+     */
+    private function isValidParentForChildNode(Root $objObject, Root $objNewObjectParent)
+    {
+        //Check if one the parents of the given node '$objObject' has ParentPathCheckActive set
+        $objParentNode = $this->getNodeWithParentPathCheckActive($objObject);
+        if ($objParentNode !== null) {
+            //Get path array of found node
+            $arrPathIds = $objParentNode->getPathArray("", SystemModule::getModuleIdByNr($objParentNode->getIntModuleNr()));
+
+            //Check, if the the new parent node '$objNewObjectParent' is somewhere within that path
+            return in_array($objNewObjectParent->getStrSystemid(), $arrPathIds);
+        }
+
+        return true;
+    }
+
 
     /**
      * Gets the first parent node which is no movable
