b3948a3bd27df7e7f054a98999f46721d37a2659
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-11-17 12:49:21 +0100

    UPDATE: module_fileindexer | config.php > update phpdoc

--- module_fileindexer/system/config/config.php
+++ module_fileindexer/system/config/config.php
@@ -7,15 +7,9 @@
 *   $Id$                                                  *
 ********************************************************************************************************/
 
-/*
-    Config-file for the ldap-connector. 
-    The sample-file is created to match the structure of an ms active directory.
-
-    There may be configured multiple ldap sources, each identified by the numerical array key.
-    Do not change the key as soon as the provider os being used, otherwise mapped users and groups may be wrong.
-*/
-
-
 $config = array();
 
+/**
+ * Command to execute the Tika (https://tika.apache.org/) jar
+ */
 $config["tika_exec"] = "java -jar " . __DIR__ . "/../../../../bin/tika-app-1.16.jar";
