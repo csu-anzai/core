b307d90b91c0efd1cfd4f7b95e88a202199ec12e
Stefan Idler <sidler@mulchprod.de>
2017-12-09 22:39:56 +0100

    BUG: system | Rights -> fixed a possible NpE
    (cherry picked from commit 965de40)

--- module_system/system/Rights.php
+++ module_system/system/Rights.php
@@ -438,20 +438,20 @@ class Rights
         $arrRow = $this->getPlainRightRow($strSystemid);
 
         if ($strPermissionFilter != "") {
-            return array($strPermissionFilter => explode(",", $arrRow[$strPermissionFilter]));
+            return array($strPermissionFilter => explode(",", "".$arrRow[$strPermissionFilter]));
         }
 
         //Exploding the array
-        $arrReturn[self::$STR_RIGHT_VIEW]       = explode(",", $arrRow[self::$STR_RIGHT_VIEW]);
-        $arrReturn[self::$STR_RIGHT_EDIT]       = explode(",", $arrRow[self::$STR_RIGHT_EDIT]);
-        $arrReturn[self::$STR_RIGHT_DELETE]     = explode(",", $arrRow[self::$STR_RIGHT_DELETE]);
-        $arrReturn[self::$STR_RIGHT_RIGHT]      = explode(",", $arrRow[self::$STR_RIGHT_RIGHT]);
-        $arrReturn[self::$STR_RIGHT_RIGHT1]     = explode(",", $arrRow[self::$STR_RIGHT_RIGHT1]);
-        $arrReturn[self::$STR_RIGHT_RIGHT2]     = explode(",", $arrRow[self::$STR_RIGHT_RIGHT2]);
-        $arrReturn[self::$STR_RIGHT_RIGHT3]     = explode(",", $arrRow[self::$STR_RIGHT_RIGHT3]);
-        $arrReturn[self::$STR_RIGHT_RIGHT4]     = explode(",", $arrRow[self::$STR_RIGHT_RIGHT4]);
-        $arrReturn[self::$STR_RIGHT_RIGHT5]     = explode(",", $arrRow[self::$STR_RIGHT_RIGHT5]);
-        $arrReturn[self::$STR_RIGHT_CHANGELOG]  = explode(",", $arrRow[self::$STR_RIGHT_CHANGELOG]);
+        $arrReturn[self::$STR_RIGHT_VIEW]       = explode(",", "".$arrRow[self::$STR_RIGHT_VIEW]);
+        $arrReturn[self::$STR_RIGHT_EDIT]       = explode(",", "".$arrRow[self::$STR_RIGHT_EDIT]);
+        $arrReturn[self::$STR_RIGHT_DELETE]     = explode(",", "".$arrRow[self::$STR_RIGHT_DELETE]);
+        $arrReturn[self::$STR_RIGHT_RIGHT]      = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT]);
+        $arrReturn[self::$STR_RIGHT_RIGHT1]     = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT1]);
+        $arrReturn[self::$STR_RIGHT_RIGHT2]     = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT2]);
+        $arrReturn[self::$STR_RIGHT_RIGHT3]     = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT3]);
+        $arrReturn[self::$STR_RIGHT_RIGHT4]     = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT4]);
+        $arrReturn[self::$STR_RIGHT_RIGHT5]     = explode(",", "".$arrRow[self::$STR_RIGHT_RIGHT5]);
+        $arrReturn[self::$STR_RIGHT_CHANGELOG]  = explode(",", "".$arrRow[self::$STR_RIGHT_CHANGELOG]);
 
         $arrReturn[self::$STR_RIGHT_INHERIT] = (int)$arrRow[self::$STR_RIGHT_INHERIT];
 
