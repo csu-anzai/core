008d155cbc31f32d2d4a3dea0c6ba19b29a49a7a
Christoph Kappestein <christoph.kappestein@artemeon.de>
2017-12-21 13:11:36 +0100

    chore(buildfiles): add mssql config

---
+++ _buildfiles/config_mssql.php
@@ -0,0 +1,12 @@
+<?php 
+
+//database-settings
+define("DB_HOST",				                "localhost");
+define("DB_USER",                               "kajona");
+define("DB_PASS",                               "kajona");
+define("DB_DB",                                 "kajona");
+define("DB_DRIVER",                             "sqlsrv");
+
+
+ini_set("session.save_path", sys_get_temp_dir());
+ini_set("session.use_cookies", "Off");
