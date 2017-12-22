cd77e6a2af864541dd18c7e4417d7f1eed4c615b
Stefan Idler <sidler@mulchprod.de>
2017-12-22 08:57:59 +0100

    chore(docs): the build script is only linking content (poc)

--- docs/build.php
+++ docs/build.php
@@ -7,21 +7,28 @@ $strIndex = "# Index\n";
 foreach ($arrModules as $strModule) {
     $strDocPath = $strBasePath . $strModule . "/docs";
     if ($strModule[0] != "." && $strModule[0] != "_" && is_dir($strDocPath)) {
-        $strIndex.= "\n";
-        $strIndex.= "## {$strModule}\n";
-        if (!is_dir(__DIR__ . "/{$strModule}")) {
-            mkdir(__DIR__ . "/{$strModule}");
-        }
-        $arrDocs = scandir($strDocPath);
-        foreach ($arrDocs as $strFile) {
-            $strPath = $strDocPath . "/" . $strFile;
-            if ($strFile[0] != "." && is_file($strPath)) {
+
+//        if (!is_dir(__DIR__ . "/{$strModule}")) {
+//            mkdir(__DIR__ . "/{$strModule}");
+//        }
+
+        $arrDocs = array_filter(scandir($strDocPath), function($strFile) {
+            return $strFile[0] != "." && substr($strFile, -3) == ".md";
+        });
+
+        if (count($arrDocs) > 0) {
+
+            $strIndex.= "\n";
+            $strIndex.= "## {$strModule}\n";
+
+            foreach ($arrDocs as $strFile) {
+                $strPath = $strDocPath."/".$strFile;
                 $strName = pathinfo($strFile, PATHINFO_FILENAME);
-                $strIndex.= "* [{$strName}]({$strModule}/{$strFile})\n";
-                copy($strPath, __DIR__ . "/{$strModule}/{$strFile}");
+                $strIndex .= "* [{$strName}](../{$strModule}/docs/{$strFile})\n";
+//            copy($strPath, __DIR__ . "/{$strModule}/{$strFile}");
             }
+            $strIndex.= "\n";
         }
-        $strIndex.= "\n";
     }
 }
 
