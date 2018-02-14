fa5e00df7347c7a438509617e3c583b4fc9c653b
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-02-14 14:28:15 +0100

    fix(system): AdminFormgenerator use strtolower

--- module_system/admin/AdminFormgenerator.php
+++ module_system/admin/AdminFormgenerator.php
@@ -424,7 +424,7 @@ class AdminFormgenerator implements \Countable
      */
     public static final function getStrFormSentParamForObject($objModel = null)
     {
-        return "formsent_".($objModel !== null ? get_class($objModel) : "");
+        return "formsent_".($objModel !== null ? StringUtil::toLowerCase(get_class($objModel)) : "");
     }
 
 
