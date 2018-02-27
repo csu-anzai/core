e11fd7cdb5a5a299ff4a437d56b6c5647533d3f8
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-02-27 15:08:00 +0100

    feat(system) SystemChangelog add method to access old changelog model instance

--- module_system/system/SystemChangelog.php
+++ module_system/system/SystemChangelog.php
@@ -99,6 +99,15 @@ class SystemChangelog
     }
 
     /**
+     * @param VersionableInterface $objCurrentObject
+     * @return VersionableInterface
+     */
+    public static function getOldInstance(VersionableInterface $objCurrentObject)
+    {
+        return self::$arrOldInstances[$objCurrentObject->getSystemid()] ?? null;
+    }
+
+    /**
      * Resets the old values for a given object, e.g. to have a clean map on new object persits
      * @param VersionableInterface|Model $objObject
      */
