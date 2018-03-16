629bc8ce56b9b0ca777f7aaebb666021659da518
Stefan Idler <sidler@mulchprod.de>
2018-03-16 08:20:27 +0100

    fix(system): fixed an undefined variable access in RequestDispatcher :: catch

--- module_system/system/RequestDispatcher.php
+++ module_system/system/RequestDispatcher.php
@@ -192,16 +192,16 @@ class RequestDispatcher
                             }
                         } catch (\Exception $objEx) {
                             if (!($objEx instanceof Exception)) {
-                                $objException = new Exception($objEx->getMessage());
+                                $objEx = new Exception($objEx->getMessage());
                             }
-                            $objException->processException();
+                            $objEx->processException();
 
                             // Execution has to be stopped here!
                             if (ResponseObject::getInstance()->getStrStatusCode() == "" || ResponseObject::getInstance()->getStrStatusCode() == HttpStatuscodes::SC_OK) {
                                 ResponseObject::getInstance()->setStrStatusCode(HttpStatuscodes::SC_INTERNAL_SERVER_ERROR);
                             }
 
-                            $strReturn = Exception::renderException($objException);
+                            $strReturn = Exception::renderException($objEx);
                         }
 
                         //if we resulted in a redirect, rewrite it to a js based on and force the redirect on "root" level
