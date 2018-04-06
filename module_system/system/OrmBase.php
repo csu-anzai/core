942918460272670c1142ba7b872543f2a710ac3e
Stefan Idler <sidler@mulchprod.de>
2018-04-06 22:14:37 +0200

    fix(system): fixed various queries

--- module_system/system/OrmBase.php
+++ module_system/system/OrmBase.php
@@ -160,7 +160,7 @@ abstract class OrmBase
         //build the query
         $strQuery = "FROM  ".implode(", ", $arrTables)." ,
                             ".Carrier::getInstance()->getObjDB()->encloseTableName("agp_system")." AS agp_system
-                  LEFT JOIN agp_system_date AS system_date
+                  LEFT JOIN agp_system_date AS agp_system_date
                          ON system_id = system_date_id
                       WHERE 1=1
                             ".$strWhere."";
