efd783eba23a78dd2528c18bf552c3cdd12e89a9
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-04-06 17:17:08 +0200

    feat(system): bootstrap prepend our autoload to the queue

--- module_system/bootstrap.php
+++ module_system/bootstrap.php
@@ -39,8 +39,10 @@ if (!include_once __DIR__."/system/Classloader.php") {
 require_once __DIR__ . "/../module_vendor/vendor/autoload.php";
 
 // -- Auto-Loader for classes ------------------------------------------------------------------------------------------
-// Register autoloader
-spl_autoload_register(array(\Kajona\System\System\Classloader::getInstance(), "loadClass"));
+// Prepend the autoloader so that we first try to use our autoload and then fallback to composer. Currently we load more
+// classes through our autoloader, if we have moved enough classes into composer packages we might want to change this
+// order
+spl_autoload_register(array(\Kajona\System\System\Classloader::getInstance(), "loadClass"), true, true);
 
 // -- Exception handler ------------------------------------------------------------------------------------------------
 // Register global exception handler for exceptions thrown but not catched (bad style ;) )
