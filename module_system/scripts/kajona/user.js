a925bc42afb0c7b653c63aab80d9df0e44489acb
Stefan Idler <sidler@mulchprod.de>
2018-08-02 17:54:28 +0200

    refactor(user): refactored the way the group-assginments of a single user are being handled

---
+++ module_system/scripts/kajona/user.js
@@ -0,0 +1,50 @@
+/**
+ * (c) 2013-2017 by Kajona, www.kajona.de
+ * Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt
+ */
+
+/**
+ *
+ * @module user
+ */
+define("user", ["jquery", "ajax"], function($, ajax){
+
+    /** @exports user */
+    var user = {
+
+        /**
+         * Adds a user to a single group
+         * @param groupid
+         * @param userid
+         * @returns {boolean}
+         */
+        addGroupToUser: function(groupid, userid) {
+            if ($('tr[data-systemid='+groupid+']').length > 0) {
+                return true;
+            }
+
+            ajax.genericAjaxCall("user", "apiGroupMemberAdd", "&userid="+userid+"&groupid="+groupid, function(data, status) {
+                if (status == 'success') {
+                    $('.admintable').append(data.row);
+                }
+            }, null, null, 'post', 'json');
+        },
+
+        /**
+         * Removes a user from a given group
+         * @param groupid
+         * @param userid
+         */
+        removeGroupFromUser: function(groupid, userid) {
+            ajax.genericAjaxCall("user", "apiGroupMemberDelete", "&userid="+userid+"&groupid="+groupid, function(data, status) {
+                if (status == 'success') {
+                    $('tr[data-systemid='+groupid+']').closest('tbody').remove();
+                }
+            });
+        }
+
+    };
+
+    return user;
+
+});
