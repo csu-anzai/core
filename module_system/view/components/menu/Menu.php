69718bfe7895270cd60382e1a08ccc66611279b6
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-08-02 11:30:16 +0200

    feat(system): add menu component

---
+++ module_system/view/components/menu/Menu.php
@@ -0,0 +1,72 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2018 ARTEMEON                                                                                   *
+*       Published under the GNU LGPL v2.1                                                               *
+********************************************************************************************************/
+
+declare(strict_types = 1);
+
+namespace Kajona\System\View\Components\Menu;
+
+use Kajona\System\View\Components\AbstractComponent;
+
+/**
+ * Renders a simple popover
+ *
+ * @author christoph.kappestein@artemeon.de
+ * @since 7.1
+ * @componentTemplate core/module_system/view/components/menu/template.twig
+ */
+class Menu extends AbstractComponent
+{
+    /**
+     * @var array
+     */
+    private $items;
+
+    /**
+     * @param array $items
+     */
+    public function setItems(array $items)
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * @param MenuItem $item
+     */
+    public function addItem(MenuItem $item)
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * @param array $items
+     */
+    public function addItems(array $items)
+    {
+        foreach ($items as $item) {
+            $this->addItem($item);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasItems()
+    {
+        return count($this->items) > 0;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function renderComponent(): string
+    {
+        $data = [
+            "items" => $this->items,
+        ];
+
+        return $this->renderTemplate($data);
+    }
+}
---
+++ module_system/view/components/menu/MenuItem.php
@@ -0,0 +1,102 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2018 ARTEMEON                                                                                   *
+*       Published under the GNU LGPL v2.1                                                               *
+********************************************************************************************************/
+
+declare(strict_types = 1);
+
+namespace Kajona\System\View\Components\Menu;
+
+/**
+ * Menu item data object
+ *
+ * @author christoph.kappestein@artemeon.de
+ * @since 7.1
+ */
+class MenuItem
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $link;
+
+    /**
+     * @var string
+     */
+    private $onClick;
+
+    /**
+     * @var string
+     */
+    private $fullEntry;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param string $link
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOnClick()
+    {
+        return $this->onClick;
+    }
+
+    /**
+     * @param string $onClick
+     */
+    public function setOnClick($onClick)
+    {
+        $this->onClick = $onClick;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullEntry()
+    {
+        return $this->fullEntry;
+    }
+
+    /**
+     * @param string $fullEntry
+     */
+    public function setFullEntry($fullEntry)
+    {
+        $this->fullEntry = $fullEntry;
+    }
+}
