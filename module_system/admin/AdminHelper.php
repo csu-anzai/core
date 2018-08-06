e32a89392e3c7ec11c5430b32ed8be343029fbb1
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-08-06 16:22:19 +0200

    feat(system): remove requirejs config from index html

--- module_system/admin/AdminHelper.php
+++ module_system/admin/AdminHelper.php
@@ -104,47 +104,4 @@ class AdminHelper
             }
         }
     }
-
-    /**
-     * Method which generates the global requirejs config
-     *
-     * @return string
-     */
-    public function generateRequireJsConfig()
-    {
-        $arrRequireConf = BootstrapCache::getInstance()->getCacheContent(BootstrapCache::CACHE_REQUIREJS);
-        if (empty($arrRequireConf)) {
-            //base config
-            $arrRequireConf = array(
-                "baseUrl" => '_webpath_',
-                "paths" => array(),
-                "shim" => array(),
-            );
-
-            foreach (Classloader::getInstance()->getArrModules() as $strFolder => $strModule) {
-                $strJsonFile = Resourceloader::getInstance()->getAbsolutePathForModule($strModule)."/scripts/provides.json";
-                if (is_file($strJsonFile)) {
-                    $arrProvidesJs = json_decode(file_get_contents($strJsonFile), true);
-                    $strBasePath = StringUtil::substring(Resourceloader::getInstance()->getWebPathForModule($strModule), 1)."/scripts/";
-
-
-                    if (isset($arrProvidesJs["paths"]) && is_array($arrProvidesJs["paths"])) {
-                        foreach ($arrProvidesJs["paths"] as $strUniqueName => $strPath) {
-                            $arrRequireConf["paths"][$strUniqueName] = $strBasePath.$strPath;
-                        }
-                    }
-
-                    if (isset($arrProvidesJs["shim"]) && is_array($arrProvidesJs["shim"])) {
-                        foreach ($arrProvidesJs["shim"] as $strUniqueName => $strValue) {
-                            $arrRequireConf["shim"][$strUniqueName] = $strValue;
-                        }
-                    }
-                }
-            }
-
-            BootstrapCache::getInstance()->updateCache(BootstrapCache::CACHE_REQUIREJS, $arrRequireConf);
-        }
-
-        return json_encode($arrRequireConf);
-    }
 }
