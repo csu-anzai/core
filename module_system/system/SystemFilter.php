4ff003e22a432bcaa36be66618f1ac7fb7847a17
sidler <sidler@mulchprod.de>
2018-08-16 15:20:44 +0200

    fix(system): added a dummy SystemFilter to limit objectList queries to a given class

---
+++ module_system/system/SystemFilter.php
@@ -0,0 +1,40 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2014 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+
+namespace Kajona\System\System;
+
+
+/**
+ * Base filter class
+ *
+ * @author stefan.idler@artemeon.de
+*/
+class SystemFilter extends FilterBase
+{
+    /**
+     * @var string
+     * @tableColumn system.system_class
+     * @filterCompareOperator EQ
+     */
+    private $class = null;
+
+    /**
+     * @return string
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    /**
+     * @param string $class
+     */
+    public function setClass(string $class)
+    {
+        $this->class = $class;
+    }
+
+}
