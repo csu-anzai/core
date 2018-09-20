f64aaaed83147d836acf8d4b3e7158207087fdd9
Sascha Bröning <sascha.broening@artemeon.de>
2018-09-20 13:10:25 +0200

    feature(chartjs): outsourced config vars to setters

--- module_chartjs/system/GraphChartjs.php
+++ module_chartjs/system/GraphChartjs.php
@@ -47,6 +47,35 @@ class GraphChartjs implements GraphInterface
      */
     private $strChartType = 'bar';
 
+    /**
+     * Defines the type of the chart [line, bar, radar, polar area, doughnut, pie, bubble]
+     *
+     * @var integer
+     */
+    private $intBorderWidth = 1;
+
+    /**
+     * Defines the width for the canvas but ONLY if respnsive is set to FALSE
+     *
+     * @var integer
+     */
+    private $intWidth = 400;
+
+    /**
+     * Defines the height for the canvas
+     *
+     * @var integer
+     */
+    private $intHeight = 400;
+
+    /**
+     * Defines the behaviour for the chart. If responsive is set to true, the width and height specifications will be ignored
+     *
+     * @var bool
+     */
+    private $bitResponsive = true;
+
+
 
 
 
@@ -124,12 +153,63 @@ class GraphChartjs implements GraphInterface
      */
     public function renderGraph()
     {
-        $this->setStrXAxisTitle('xAxis');
-        $this->setStrYAxisTitle('yAxis');
+        $this->setStrXAxisTitle('xAxis Title');
+        $this->setStrYAxisTitle('yAxis Title');
         $this->setStrGraphTitle('ARTEMEON Graph Chart.js');
         $this->setStrChartType('bar');
+        $this->setIntBorderWidth(1);
+        $this->setBitResponsive(true);
 
-        $strReturn = '<canvas id="chartCanvas" height="600"></canvas>';
+        $arrayColors = [
+            0 => ['name' => 'Absolute Zero', 'hex' => '#0048Ba', 'rgba' => '0, 72,186'],
+            1 => ['name' => 'Acid', 'hex' => '#B0BF1A', 'rgba' => '176, 191 26'],
+            2 => ['name' => 'Alloy Orange', 'hex' => '#C46210', 'rgba' => '196, 98 16'],
+            3 => ['name' => 'Amber', 'hex' => '#FFBF00', 'rgba' => '255, 191, 0'],
+            4 => ['name' => 'Amethyst', 'hex' => '#9966CC', 'rgba' => '153, 102, 204'],
+            5 => ['name' => 'Antique ruby', 'hex' => '#841B2D', 'rgba' => '132, 27, 45'],
+            6 => ['name' => 'Antique white', 'hex' => '#FAEBD7', 'rgba' => '250, 235, 215'],
+            7 => ['name' => 'Apple green', 'hex' => '#8DB600', 'rgba' => '141, 182, 0'],
+            8 => ['name' => 'Arctic lime', 'hex' => '#D0FF14', 'rgba' => '208, 255, 20'],
+            9 => ['name' => 'Atomic tangerine', 'hex' => '#FF9966', 'rgba' => '255, 153, 102'],
+            10 => ['name' => 'Azure', 'hex' => '#007FFF', 'rgba' => '0, 127, 255'],
+            11 => ['name' => 'Baker-Miller pink', 'hex' => '#FF91AF', 'rgba' => '255, 145, 175'],
+            12 => ['name' => 'Barbie pink', 'hex' => '#E94196', 'rgba' => '233, 65, 150'],
+            13 => ['name' => 'Bitter lemon', 'hex' => '#CAE00D', 'rgba' => '202, 224, 13'],
+            14 => ['name' => 'Black coral', 'hex' => '#54626F', 'rgba' => '84, 98, 111']
+        ];
+
+        $labels = [];
+        $backgroundColors = [];
+        $borderColors = [];
+        $blockedIndex = [];
+        for ($i = 0; $i <= 5; $i++) {
+            $rng = rand(0, 14);
+            while(in_array($rng, $blockedIndex)){
+                $rng = rand(0, 14);
+            }
+
+            array_push($blockedIndex, $rng);
+            array_push($labels, $arrayColors[$rng]['name']);
+            array_push($backgroundColors, 'rgba(' . $arrayColors[$rng]['rgba'] . ', 0.3)');
+            array_push($borderColors, $arrayColors[$rng]['hex']);
+        }
+
+        $data = [rand(2, 20), rand(5, 15), rand(3, 18), rand(6, 20), rand(4, 17), rand(1, 20)];
+        $jsonData = $this->getJsonFormat($data);
+
+        $jsonLabels = $this->getJsonFormat($labels);
+        $jsonBackgroundColors = $this->getJsonFormat($backgroundColors);
+        $jsonBorderColors = $this->getJsonFormat($borderColors);
+
+        $finalData = [
+            'datasetLabel' => 'Dataset 1',
+            'labels' => $jsonLabels,
+            'data' => $jsonData,
+            'backgroundColor' => $jsonBackgroundColors,
+            'borderColor' => $jsonBorderColors
+        ];
+
+        $strReturn = '<canvas id="chartCanvas" width="' . $this->intWidth . '" height="' . $this->intHeight . '"></canvas>';
         $strReturn .= "<script type='text/javascript'>
 
             require(['chartjs'], function(chartjs) {
@@ -137,65 +217,30 @@ class GraphChartjs implements GraphInterface
                 var ctx = document.getElementById('chartCanvas').getContext('2d');
                 var myChart = new chartjs.Chart(ctx, {
                     type: '{$this->strChartType}',
-                    data: {
-                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    data: 
+                    {
+                        labels: {$finalData["labels"]},
                         datasets: [
                             {
-                            label: 'Dataset 1',
-                            data: [12, 19, 8, 5, 7, 3],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255,99,132,1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
-                            ],
-                            hoverBackgroundColor: 'rgba(232,105,90,0.8)',
-                            scaleStepWidth: 1,
-                            borderWidth: 1
-                        },
-                        {
-                            label: 'Dataset 2',
-                            data: [4, 14, 18, 14, 5, 5],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255,99,132,1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
-                            ],
-                            borderWidth: 1
-                        }
+                                label: '{$finalData["datasetLabel"]}',
+                                data: {$finalData["data"]},
+                                backgroundColor: {$finalData["backgroundColor"]},
+                                borderColor: {$finalData["borderColor"]},
+                                borderWidth: {$this->intBorderWidth}
+                            }
                         ]
-                    },
+                    },                    
                     options: {
                         title: {
                             display: true,
                             text: '{$this->strGraphTitle}'
                         },
-                        responsive: true,
+                        responsive: {$this->bitResponsive},
                         maintainAspectRatio: false,
                         scales: {
                             yAxes: [{
                                 ticks: {
+                                    min: 0,
                                     callback: function(value, index, values) {
                                         return '# ' + value;
                                     }
@@ -210,7 +255,7 @@ class GraphChartjs implements GraphInterface
                                     fontColor: 'blue',
                                     fontSize: 14,
                                     stepSize: 1,
-                                    beginAtZero:true
+                                    beginAtZero: true
                                 },
                                 scaleLabel: {
                                     display: true,
@@ -224,6 +269,15 @@ class GraphChartjs implements GraphInterface
         </script>";
 
         return $strReturn;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    private function getJsonFormat($data)
+    {
+        return is_string($data) && is_array(json_decode($data, true)) && (json_last_error() == JSON_ERROR_NONE) ? $data : json_encode($data);
     }
 
     /**
@@ -270,9 +324,6 @@ class GraphChartjs implements GraphInterface
 
 
 
-
-
-
     /**
      * @param string $strColor
      *
@@ -290,7 +341,7 @@ class GraphChartjs implements GraphInterface
      */
     public function setIntWidth($intWidth)
     {
-
+        $this->intWidth = $intWidth;
     }
 
     /**
@@ -300,7 +351,7 @@ class GraphChartjs implements GraphInterface
      */
     public function setIntHeight($intHeight)
     {
-
+        $this->intHeight = $intHeight;
     }
 
     /**
@@ -363,6 +414,22 @@ class GraphChartjs implements GraphInterface
     public function setArrSeriesColors($arrSeriesColors)
     {
         return '';
+    }
+
+    /**
+     * @param int $intBorderWidth
+     */
+    public function setIntBorderWidth(int $intBorderWidth)
+    {
+        $this->intBorderWidth = $intBorderWidth;
+    }
+
+    /**
+     * @param int $bitResponsive
+     */
+    public function setBitResponsive(int $bitResponsive)
+    {
+        $this->bitResponsive = $bitResponsive;
     }
 
 }
