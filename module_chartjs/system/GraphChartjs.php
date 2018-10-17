273f0c2d442d8158a5bd091b303e83e00011d41c
andrii.konoval <andrii.konoval@artemeon.de>
2018-10-17 16:47:47 +0200

    fix(chartjs): Moved down renderGraph method

--- module_chartjs/system/GraphChartjs.php
+++ module_chartjs/system/GraphChartjs.php
@@ -253,109 +253,6 @@ class GraphChartjs implements GraphInterface
     }
 
     /**
-     * @return mixed|string
-     *
-     * @see GraphInterface::renderGraph()
-     */
-    public function renderGraph()
-    {
-        if (!isset($this->arrChartData['data']) || count($this->arrChartData['data']) == 0) {
-            throw new Exception("Chart not initialized yet", Exception::$level_ERROR);
-        }
-
-        if (!isset($this->arrChartData['data']['labels']) || count($this->arrChartData['data']['labels']) == 0) {
-            $this->arrChartData['data']['labels'] = range(1, $this->intXLabelsCount);
-        }
-
-        $strSystemId = generateSystemid();
-        $strResizeableId = "resize_".$strSystemId;
-        $strChartId = "chart_".$strSystemId;
-        $strLinkExportId = $strChartId."_exportlink";
-
-        $strWidth = $this->isBitResponsive() ? "100%" : $this->intWidth."px";
-        $strReturn = "<div onmouseover='$(\"#$strLinkExportId\").show();' onmouseout='$(\"#$strLinkExportId\").hide();' id=\"$strResizeableId\" style=\"width:{$strWidth}; height:".$this->intHeight."px;\">";
-        $strReturn .= '<canvas id="'.$strChartId.'" width="'.$this->intWidth.'" height="'.$this->intHeight.'"></canvas>';
-        if ($this->isBitDownloadLink()) {
-            $strImage = AdminskinHelper::getAdminImage("icon_downloads", Carrier::getInstance()->getObjLang()->getLang("commons_save_as_image", "system"));
-            $strReturn .= "<div class=\"chartjs-link-bar\"><a class=\"chartjs-image-link\" id=\"$strLinkExportId\" download>$strImage</a></div>";
-            $this->arrChartGlobalOptions['createImageLink'] = true;
-        }
-        $strReturn .= "</div>";
-
-        $strReturn .= "<script type='text/javascript'>
-
-            require(['chartjs', 'chartjsHelper'], function(chartjs, chartjsHelper) {
-                require(['chartjs-plugin-datalabels'], function(chartjsDatalabels) {
-		            var chartData = ".json_encode($this->arrChartData, JSON_NUMERIC_CHECK).";
-    		        var chartGlobalOptions = ".json_encode($this->arrChartGlobalOptions, JSON_NUMERIC_CHECK).";
-                    var ctx = document.getElementById('".$strChartId."');
-                
-                    ctx.style.backgroundColor = chartGlobalOptions['backgroundColor'];
-                    Chart.defaults.global.defaultFontColor = typeof (chartGlobalOptions['defaultFontColor']) !== 'undefined' ? chartGlobalOptions['defaultFontColor'] : 'black';
-                    Chart.defaults.global.defaultFontFamily = chartGlobalOptions['defaultFontFamily'];
-                    Chart.defaults.global.legend.labels.fontColor = typeof (chartGlobalOptions['labelsFontColor']) !== 'undefined' ? chartGlobalOptions['labelsFontColor'] : chartGlobalOptions['defaultFontColor'];
-                
-                    if (typeof (chartGlobalOptions['setDefaultTooltip']) == 'undefined' || !chartGlobalOptions['setDefaultTooltip']) {
-                        chartData['options']['tooltips'] = {
-                            enabled: true,
-                            mode: 'single',
-                            callbacks: {
-                                label: function(tooltipItems, data) {
-                                    return data.datasets[tooltipItems.datasetIndex].label  + ' : ' + data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index];
-                                }
-                            }
-                        };
-                    }
-                
-                    if (typeof (chartGlobalOptions['createImageLink']) !== 'undefined' || chartGlobalOptions['createImageLink']) {
-                       chartData['options']['animation'] = {
-                            onComplete: createExportLink
-                       }
-                    }
-
-                    if (typeof (chartGlobalOptions['notShowNullValues']) !== 'undefined' || chartGlobalOptions['notShowNullValues']) {
-                       chartData['options']['plugins']['datalabels'] = {
-                          formatter: (value, ctx) => {
-                             return chartjsHelper.dataNotShowNullValues(value);
-                          }
-                       }
-                    }
-
-                    if (typeof (chartGlobalOptions['percentageValues']) !== 'undefined' || chartGlobalOptions['percentageValues']) {
-                       chartData['options']['plugins']['datalabels'] = {
-                          formatter: (value, ctx) => {
-                             return chartjsHelper.dataShowPercentage(value, ctx);
-                          }
-                       }
-                    }
-
-                    chartData['options']['onClick'] = function (evt){
-                        var item = this.getElementAtEvent(evt)[0];
-                        if (typeof item !== 'undefined') {
-                            var datasetIndex = item._datasetIndex;
-                            var index = item._index; 
-                            chartjsHelper.onClickHandler(evt, index, datasetIndex, chartData['data']['datasets'][datasetIndex]['dataPoints'][index]);
-                        } 
-                    };
-
-                    var myChart = new chartjs.Chart(ctx, {
-                        type: chartData['type'],
-                        data : chartData['data'],
-                        options : chartData['options'],
-                    });     
-                    
-                    function createExportLink() {
-                        var url = myChart.toBase64Image();
-                        document.getElementById('".$strLinkExportId."').href = url;
-                    }
-                });
-            });
-        </script>";
-
-        return $strReturn;
-    }
-
-    /**
      * @param string $strTitle
      *
      * @see GraphInterface::setStrXAxisTitle()
@@ -651,5 +548,108 @@ class GraphChartjs implements GraphInterface
     public function setDefaultTooltip($bitSetDefaultTooltip = true)
     {
         $this->arrChartGlobalOptions['setDefaultTooltip'] = $bitSetDefaultTooltip;
+    }
+
+    /**
+     * @return mixed|string
+     *
+     * @see GraphInterface::renderGraph()
+     */
+    public function renderGraph()
+    {
+        if (!isset($this->arrChartData['data']) || count($this->arrChartData['data']) == 0) {
+            throw new Exception("Chart not initialized yet", Exception::$level_ERROR);
+        }
+
+        if (!isset($this->arrChartData['data']['labels']) || count($this->arrChartData['data']['labels']) == 0) {
+            $this->arrChartData['data']['labels'] = range(1, $this->intXLabelsCount);
+        }
+
+        $strSystemId = generateSystemid();
+        $strResizeableId = "resize_".$strSystemId;
+        $strChartId = "chart_".$strSystemId;
+        $strLinkExportId = $strChartId."_exportlink";
+
+        $strWidth = $this->isBitResponsive() ? "100%" : $this->intWidth."px";
+        $strReturn = "<div onmouseover='$(\"#$strLinkExportId\").show();' onmouseout='$(\"#$strLinkExportId\").hide();' id=\"$strResizeableId\" style=\"width:{$strWidth}; height:".$this->intHeight."px;\">";
+        $strReturn .= '<canvas id="'.$strChartId.'" width="'.$this->intWidth.'" height="'.$this->intHeight.'"></canvas>';
+        if ($this->isBitDownloadLink()) {
+            $strImage = AdminskinHelper::getAdminImage("icon_downloads", Carrier::getInstance()->getObjLang()->getLang("commons_save_as_image", "system"));
+            $strReturn .= "<div class=\"chartjs-link-bar\"><a class=\"chartjs-image-link\" id=\"$strLinkExportId\" download>$strImage</a></div>";
+            $this->arrChartGlobalOptions['createImageLink'] = true;
+        }
+        $strReturn .= "</div>";
+
+        $strReturn .= "<script type='text/javascript'>
+
+            require(['chartjs', 'chartjsHelper'], function(chartjs, chartjsHelper) {
+                require(['chartjs-plugin-datalabels'], function(chartjsDatalabels) {
+		            var chartData = ".json_encode($this->arrChartData, JSON_NUMERIC_CHECK).";
+    		        var chartGlobalOptions = ".json_encode($this->arrChartGlobalOptions, JSON_NUMERIC_CHECK).";
+                    var ctx = document.getElementById('".$strChartId."');
+                
+                    ctx.style.backgroundColor = chartGlobalOptions['backgroundColor'];
+                    Chart.defaults.global.defaultFontColor = typeof (chartGlobalOptions['defaultFontColor']) !== 'undefined' ? chartGlobalOptions['defaultFontColor'] : 'black';
+                    Chart.defaults.global.defaultFontFamily = chartGlobalOptions['defaultFontFamily'];
+                    Chart.defaults.global.legend.labels.fontColor = typeof (chartGlobalOptions['labelsFontColor']) !== 'undefined' ? chartGlobalOptions['labelsFontColor'] : chartGlobalOptions['defaultFontColor'];
+                
+                    if (typeof (chartGlobalOptions['setDefaultTooltip']) == 'undefined' || !chartGlobalOptions['setDefaultTooltip']) {
+                        chartData['options']['tooltips'] = {
+                            enabled: true,
+                            mode: 'single',
+                            callbacks: {
+                                label: function(tooltipItems, data) {
+                                    return data.datasets[tooltipItems.datasetIndex].label  + ' : ' + data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index];
+                                }
+                            }
+                        };
+                    }
+                
+                    if (typeof (chartGlobalOptions['createImageLink']) !== 'undefined' || chartGlobalOptions['createImageLink']) {
+                       chartData['options']['animation'] = {
+                            onComplete: createExportLink
+                       }
+                    }
+
+                    if (typeof (chartGlobalOptions['notShowNullValues']) !== 'undefined' || chartGlobalOptions['notShowNullValues']) {
+                       chartData['options']['plugins']['datalabels'] = {
+                          formatter: (value, ctx) => {
+                             return chartjsHelper.dataNotShowNullValues(value);
+                          }
+                       }
+                    }
+
+                    if (typeof (chartGlobalOptions['percentageValues']) !== 'undefined' || chartGlobalOptions['percentageValues']) {
+                       chartData['options']['plugins']['datalabels'] = {
+                          formatter: (value, ctx) => {
+                             return chartjsHelper.dataShowPercentage(value, ctx);
+                          }
+                       }
+                    }
+
+                    chartData['options']['onClick'] = function (evt){
+                        var item = this.getElementAtEvent(evt)[0];
+                        if (typeof item !== 'undefined') {
+                            var datasetIndex = item._datasetIndex;
+                            var index = item._index; 
+                            chartjsHelper.onClickHandler(evt, index, datasetIndex, chartData['data']['datasets'][datasetIndex]['dataPoints'][index]);
+                        } 
+                    };
+
+                    var myChart = new chartjs.Chart(ctx, {
+                        type: chartData['type'],
+                        data : chartData['data'],
+                        options : chartData['options'],
+                    });     
+                    
+                    function createExportLink() {
+                        var url = myChart.toBase64Image();
+                        document.getElementById('".$strLinkExportId."').href = url;
+                    }
+                });
+            });
+        </script>";
+
+        return $strReturn;
     }
 }
