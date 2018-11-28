054ce577b99d4eda6e994cebb5407a8573fd8770
andrii.konoval <andrii.konoval@artemeon.de>
2018-11-28 17:08:49 +0100

    feat(prozessverwaltung, leistungsscheine, slareporting, system): Changed default order of elements in StackDataBar componente

--- module_system/view/components/stackeddatabar/StackedDataBar.php
+++ module_system/view/components/stackeddatabar/StackedDataBar.php
@@ -78,11 +78,11 @@ class StackedDataBar extends AbstractComponent
         $colorSet = count($this->colors) >= count($this->data) ? $this->colors : $this->defaultColors;
         $dataSum = 0;
         $uniqueKey = md5(microtime().rand());
-        foreach (array_reverse($this->data) as $index => $objDataPiont) {
+        foreach ($this->data as $index => $objDataPiont) {
             $dataValue = $objDataPiont->getFloatValue();
             $dataSum += $dataValue;
             if (!empty($objDataPiont)) {
-                $resultData[] = [
+                $resultData[$index] = [
                     'value' => round($dataValue, 2),
                     'color' => $colorSet[$index],
                     'label' => isset($this->labels[$index]) ? $this->labels[$index] : "",
