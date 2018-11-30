379a5b343a5dd86ca2abc350559d59a9685ce995
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-11-30 14:51:03 +0100

    feat(system): MessagingNotification add notification class

---
+++ module_system/system/MessagingNotification.php
@@ -0,0 +1,16 @@
+<?php
+/*"******************************************************************************************************
+*   (c) 2007-2016 by Kajona, www.kajona.de                                                              *
+*       Published under the GNU LGPL v2.1, see /system/licence_lgpl.txt                                 *
+********************************************************************************************************/
+declare(strict_types=1);
+
+namespace Kajona\System\System;
+
+/**
+ * @author christoph.kappestein@artemeon.de
+ * @since 7.1
+ */
+class MessagingNotification extends MessagingAlert
+{
+}
