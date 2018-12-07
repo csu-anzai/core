b2bbe9ff3c958bf0320bf6ca60ea781307334b55
Christoph Kappestein <christoph.kappestein@artemeon.de>
2018-12-07 17:21:04 +0100

    feat(system): Dropdown add new dropdown component

---
+++ module_system/view/components/formentry/dropdown/Dropdown.php
@@ -0,0 +1,88 @@
+<?php
+/*"******************************************************************************************************
+ *   (c) ARTEMEON Management Partner GmbH
+ *       Published under the GNU LGPL v2.1
+ ********************************************************************************************************/
+
+declare(strict_types=1);
+
+namespace Kajona\System\View\Components\Formentry\Dropdown;
+
+use Kajona\System\View\Components\Formentry\FormentryComponentAbstract;
+
+/**
+ * Dropdown
+ *
+ * @author christoph.kappestein@artemeon.de
+ * @since 7.1
+ * @componentTemplate core/module_system/view/components/formentry/dropdown/template.twig
+ */
+class Dropdown extends FormentryComponentAbstract
+{
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var string
+     */
+    protected $selected;
+
+    /**
+     * @var string
+     */
+    protected $addons;
+
+    /**
+     * @param string $name
+     * @param string $title
+     * @param array $options
+     * @param string $selected
+     */
+    public function __construct(string $name, string $title, array $options, $selected = null)
+    {
+        parent::__construct($name, $title);
+
+        $this->options = $options;
+        $this->selected = $selected;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @param string $selected
+     */
+    public function setSelected(string $selected)
+    {
+        $this->selected = $selected;
+    }
+
+    /**
+     * @param string $selected
+     * @deprecated
+     */
+    public function setAddons($addons)
+    {
+        $this->addons = $addons;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function buildContext()
+    {
+        $context = parent::buildContext();
+        $context["options"] = $this->options;
+        $context["selected"] = $this->selected;
+        $context["addons"] = $this->addons;
+
+        return $context;
+    }
+}
