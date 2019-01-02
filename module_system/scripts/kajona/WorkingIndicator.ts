e2c99f7e52e71c90498dd1527abd63d37b467af7
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 13:12:10 +0100

    feat(system): migrated working indicator to typescript

---
+++ module_system/scripts/kajona/WorkingIndicator.ts
@@ -0,0 +1,31 @@
+///<reference path="../defs/jquery.d.ts" />
+///<amd-module name="workingIndicator"/>
+
+import * as $ from "jquery";
+
+class WorkingIndicator {
+
+    private static intWorkingCount = 0;
+
+    public static start() {
+        if(this.intWorkingCount == 0) {
+            $('#status-indicator').addClass("active");
+        }
+        this.intWorkingCount++;
+    }
+
+    public static stop() {
+        this.intWorkingCount--;
+
+        if(this.intWorkingCount == 0) {
+            $('#status-indicator').removeClass("active");
+        }
+    }
+
+    public static getInstance() {
+        return WorkingIndicator;
+    }
+
+}
+
+export = WorkingIndicator
