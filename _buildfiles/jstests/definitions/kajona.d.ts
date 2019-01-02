e6eda0b4762b0cf1ea7dbb4ad5a329e3b588fd58
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 16:49:22 +0100

    feat(_buildfiles): kajona.d.ts add bootstrap definition

--- _buildfiles/jstests/definitions/kajona.d.ts
+++ _buildfiles/jstests/definitions/kajona.d.ts
@@ -3,6 +3,7 @@
 ///<reference path="../node_modules/@types/toastr/index.d.ts" />
 ///<reference path="../node_modules/@types/qtip2/index.d.ts" />
 ///<reference path="../node_modules/@types/requirejs/index.d.ts" />
+///<reference path="../node_modules/@types/bootstrap/index.d.ts" />
 
 declare module 'jquery' {
     export = jQuery;
