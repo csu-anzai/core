4dc6d0220ed90162cee6fabdd789f4e42d050be5
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 16:50:18 +0100

    feat(system): Quickhelp.ts migrate quickhelp to typescript

---
+++ module_system/scripts/kajona/Quickhelp.ts
@@ -0,0 +1,30 @@
+///<reference path="../../../_buildfiles/jstests/definitions/kajona.d.ts" />
+///<amd-module name="quickhelp"/>
+
+import * as $ from "jquery";
+import "bootstrap";
+
+/**
+ * Module to handle the general quickhelp entry
+ */
+class Quickhelp {
+
+    public static setQuickhelp(strTitle: string, strText: string) {
+        if(strText.trim() == "" ) {
+            return;
+        }
+        $('#quickhelp').popover({
+            title: strTitle,
+            content: strText,
+            placement: 'bottom',
+            trigger: 'hover',
+            html: true
+        }).css("cursor", "help").show();
+
+    };
+
+    public static resetQuickhelp() {
+        $('#quickhelp').hide().popover('destroy');
+    };
+
+}
