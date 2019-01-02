d7756c393450864f18baba06eda9ac96dbc6973a
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 15:45:20 +0100

    feat(_buildfiles): kajona.d.ts add requirejs type definition

--- _buildfiles/jstests/definitions/kajona.d.ts
+++ _buildfiles/jstests/definitions/kajona.d.ts
@@ -2,6 +2,7 @@
 ///<reference path="../node_modules/@types/ckeditor/index.d.ts" />
 ///<reference path="../node_modules/@types/toastr/index.d.ts" />
 ///<reference path="../node_modules/@types/qtip2/index.d.ts" />
+///<reference path="../node_modules/@types/requirejs/index.d.ts" />
 
 declare module 'jquery' {
     export = jQuery;
