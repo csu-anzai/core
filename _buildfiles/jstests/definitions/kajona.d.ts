71527639af22ba42be99c79cccb4f32cebea5eec
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 13:37:25 +0100

    feat(_buildfiles): kajona.d.ts add global definition which includes all external definitions

---
+++ _buildfiles/jstests/definitions/kajona.d.ts
@@ -0,0 +1,7 @@
+///<reference path="../node_modules/@types/jquery/index.d.ts" />
+///<reference path="../../../_buildfiles/jstests/node_modules/@types/ckeditor/index.d.ts" />
+///<reference path="../../../_buildfiles/jstests/node_modules/@types/toastr/index.d.ts" />
+
+declare module 'jquery' {
+    export = jQuery;
+}
