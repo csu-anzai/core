aca5bfd6a5cb0c3ec543dc1c95a54586f7f82e23
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-02 15:05:52 +0100

    feat(_buildfiles): add qtip type definition

--- _buildfiles/jstests/definitions/kajona.d.ts
+++ _buildfiles/jstests/definitions/kajona.d.ts
@@ -1,6 +1,7 @@
 ///<reference path="../node_modules/@types/jquery/index.d.ts" />
 ///<reference path="../node_modules/@types/ckeditor/index.d.ts" />
 ///<reference path="../node_modules/@types/toastr/index.d.ts" />
+///<reference path="../node_modules/@types/qtip2/index.d.ts" />
 
 declare module 'jquery' {
     export = jQuery;
