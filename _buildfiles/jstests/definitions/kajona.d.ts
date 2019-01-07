ee54f4ad3bc6d967d3d5adc18a813a8d16d96662
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-07 14:55:09 +0100

    feat(_buildfiles): add custom jstree definition

--- _buildfiles/jstests/definitions/kajona.d.ts
+++ _buildfiles/jstests/definitions/kajona.d.ts
@@ -7,9 +7,9 @@
 ///<reference path="../node_modules/@types/jqueryui/index.d.ts" />
 ///<reference path="../node_modules/@types/d3/index.d.ts" />
 ///<reference path="../node_modules/@types/jquery.fileupload/index.d.ts" />
-///<reference path="../node_modules/@types/jstree/index.d.ts" />
-///<reference path="moment.d.ts" />
 ///<reference path="calendarheatmap.d.ts" />
+///<reference path="jstree.ts" />
+///<reference path="moment.d.ts" />
 
 declare module 'jquery' {
     export = jQuery;
