9194269bee14871bd8fcb24d7ca78cb6f5ba3131
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-01-31 14:24:59 +0100

    feat(system): add debug script to show all tables which are not in sync with the entities

---
+++ module_system/debug/entity_table_diff.php
@@ -0,0 +1,56 @@
+<?php
+
+$connection = \Kajona\System\System\Database::getInstance();
+$orm = new \Kajona\System\System\OrmSchemamanager();
+$allTables = [];
+$classes = getAllEntities();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($classes as $class) {
+        $orm->updateTable($class);
+    }
+} else {
+    foreach ($classes as $class) {
+        $tables = $orm->getTableDefinitions($class);
+        foreach ($tables as $table) {
+            /** @var \Kajona\System\System\OrmSchemamanagerTable $table */
+            if ($connection->hasTable($table->getStrName())) {
+                foreach ($table->getArrRows() as $column) {
+                    if (!$connection->hasColumn($table->getStrName(), $column->getStrName())) {
+                        echo "<b>Column {$column->getStrName()} does not exist on table {$table}</b>\n";
+                    }
+                }
+            } else {
+                echo "<b>Table {$table->getStrName()} does not exist</b>\n";
+            }
+
+            $allTables[] = $table->getStrName();
+        }
+    }
+
+    $allTables = array_unique($allTables);
+
+    echo "\n";
+    echo "Found " . count($classes) . " entities and " . count($allTables) . " tables\n";
+}
+
+echo "\n";
+echo "<form method='POST'>";
+echo "<input type='submit' value='Update Tables'>";
+echo "</form>";
+
+function getAllEntities() {
+    $filter = function (&$strOneFile, $strPath) {
+        $instance = \Kajona\System\System\Classloader::getInstance()->getInstanceFromFilename($strPath, \Kajona\System\System\Root::class);
+        if ($instance instanceof \Kajona\System\System\Root) {
+            $strOneFile = get_class($instance);
+        } else {
+            $strOneFile = null;
+        }
+    };
+
+    $classes = \Kajona\System\System\Resourceloader::getInstance()->getFolderContent("/system", array(".php"), false, null, $filter);
+    $classes = array_filter($classes);
+
+    return $classes;
+}
