4f3c71b822b502d9aacc1e75df56056bc0ae84bb
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-02-18 16:39:05 +0100

    feat(dashboard): DashboardAdmin pass available params to dashboard menu to not lose existing params on switch

--- module_dashboard/admin/DashboardAdmin.php
+++ module_dashboard/admin/DashboardAdmin.php
@@ -161,10 +161,13 @@ class DashboardAdmin extends AdminEvensimpler implements AdminInterface
         //add a toolbar
         $return .= $this->objToolkit->addToContentToolbar(Link::getLinkAdminDialog("dashboard", "listWidgets", [], $this->getLang("action_add_widget_to_dashboard"), $this->getLang("action_add_widget_to_dashboard"), "icon_new"));
 
+        $params = Carrier::getAllParams();
+        unset($params["module"]);
+        unset($params["action"]);
 
         $menu = new DynamicMenu(
             "{$cfg->getStrDisplayName()}<i class='fa fa-caret-down'></i>",
-            Link::getLinkAdminXml("dashboard", "apiGetDashboardMenu", [])
+            Link::getLinkAdminXml("dashboard", "apiGetDashboardMenu", $params)
         );
 
         $menu = $menu->renderComponent();
@@ -188,12 +191,20 @@ class DashboardAdmin extends AdminEvensimpler implements AdminInterface
         $dd = [];
         $items = [];
 
+        $params = Carrier::getAllParams();
+        unset($params["admin"]);
+        unset($params["contentFill"]);
+        unset($params["module"]);
+        unset($params["action"]);
+
         $return = "";
         /** @var DashboardConfig $singleCfg */
         foreach (DashboardConfig::getObjectListFiltered(null, $root->getSystemid()) as $singleCfg) {
             $dd[$singleCfg->getSystemid()] = $singleCfg->getStrDisplayName();
 
-            $text = new Text(Link::getLinkAdmin("dashboard", "list", ["configid" => $singleCfg->getSystemid()], $this->objToolkit->listButton(AdminskinHelper::getAdminImage("icon_dashboard"))." ".$singleCfg->getStrDisplayName(), "", "", false));
+            $params["configid"] = $singleCfg->getSystemid();
+
+            $text = new Text(Link::getLinkAdmin("dashboard", "list", $params, $this->objToolkit->listButton(AdminskinHelper::getAdminImage("icon_dashboard"))." ".$singleCfg->getStrDisplayName(), "", "", false));
             $items[] = $text;
         }
 
