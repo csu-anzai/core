441518240e98899f839a3660aa7bf37ba99286a5
dhafer.harrathi <vQvycMH48;8h%G>
2019-04-01 14:29:34 +0200

    feat(webpack):working on adding vue to webpack and tsconfig

--- _buildfilesNew/testGlob.js
+++
@@ -1,7 +0,0 @@
-const glob = require('glob')
-// glob('../../{core,core_agp}/module_*/scripts/*/*.ts', function (er, files) {
-const tsPaths = glob.sync('../../{core,core_agp}/module_*/scripts', {
-  realpath: true
-})
-
-console.log('paths : ', tsPaths)
--- _buildfilesNew/webpack.config.js
+++ _buildfilesNew/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
-const glob = require("glob");
-var LiveReloadPlugin = require("webpack-livereload-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const Dotenv = require('dotenv-webpack')
+const glob = require('glob')
+var LiveReloadPlugin = require('webpack-livereload-plugin')
 
 const liveReloadOptions = {
-  hostname: "localhost",
-  protocol: "http"
-};
-const devMode = process.env.NODE_ENV !== "production";
+  hostname: 'localhost',
+  protocol: 'http'
+}
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   entry: {
-    agp: glob.sync("../../{core,core_agp}/module_*/scripts/**/*.ts")
+    agp: glob.sync('../../{core,core_agp}/module_*/scripts/**/*.ts')
   },
   output: {
-    filename: "./[name].min.js",
-    path: path.resolve(__dirname, "../module_system/scripts/")
+    filename: './[name].min.js',
+    path: path.resolve(__dirname, '../module_system/scripts/')
   },
 
   module: {
@@ -24,71 +24,70 @@ module.exports = {
       {
         test: /\.vue$/,
 
-        loader: "vue-loader",
+        loader: 'vue-loader',
         options: {
           loaders: {
-            scss: "vue-style-loader!css-loader!sass-loader",
-            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
         }
       },
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-
+        // loader: "awesome-typescript-loader",
+        loader: 'ts-loader',
         options: {
-          // appendTsSuffixTo: [/\.vue$/]
+          // appendTsSuffixTo: [/\.vue$/],
           appendTsSuffixTo: [/\.ts\.vue$/],
-          appendTsxSuffixTo: [/\.tsx\.vue$/],
-          configFile: "./tsconfig.json"
+          // appendTsxSuffixTo: [/\.tsx\.vue$/],
+          configFile: path.resolve(__dirname, './tsconfig.json')
         },
         exclude: /node_modules/
       },
 
       {
         test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.png($|\?)/,
-        loader: "url-loader"
+        loader: 'url-loader'
       },
       {
         test: /\.less$/,
         use: [
           {
-            loader: "style-loader" // creates style nodes from JS strings
+            loader: 'style-loader' // creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // translates CSS into CommonJS
+            loader: 'css-loader' // translates CSS into CommonJS
           },
           {
-            loader: "less-loader" // compiles Less to CSS
+            loader: 'less-loader' // compiles Less to CSS
           }
         ]
       }
     ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, "./node_modules")],
-    extensions: [".ts", ".js", ".vue", ".json"],
+    modules: [path.resolve(__dirname, './node_modules')],
+    extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
-      vue$: "vue/dist/vue.esm.js",
-      "load-image": "blueimp-load-image/js/load-image.js",
-      "load-image-meta": "blueimp-load-image/js/load-image-meta.js",
-      "load-image-exif": "blueimp-load-image/js/load-image-exif.js",
-      "load-image-scale": "blueimp-load-image/js/load-image-scale.js",
-      "canvas-to-blob": "blueimp-canvas-to-blob/js/canvas-to-blob.js",
-      "jquery-ui/ui/widget": "blueimp-file-upload/js/vendor/jquery.ui.widget.js"
+      vue$: 'vue/dist/vue.esm.js',
+      'load-image': 'blueimp-load-image/js/load-image.js',
+      'load-image-meta': 'blueimp-load-image/js/load-image-meta.js',
+      'load-image-exif': 'blueimp-load-image/js/load-image-exif.js',
+      'load-image-scale': 'blueimp-load-image/js/load-image-scale.js',
+      'canvas-to-blob': 'blueimp-canvas-to-blob/js/canvas-to-blob.js',
+      'jquery-ui/ui/widget': 'blueimp-file-upload/js/vendor/jquery.ui.widget.js'
     }
   },
   plugins: [
     new Dotenv({
-      path: path.resolve(__dirname, ".env.dev")
+      path: path.resolve(__dirname, '.env.dev')
     }),
     new webpack.ProvidePlugin({
-      jQuery: "jquery",
-      $: "jquery",
-      jquery: "jquery",
-      mermaid: "mermaid"
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery'
     }),
     new LiveReloadPlugin(liveReloadOptions)
-  ],
-  watch: true
-};
+  ]
+  // watch: true
+}
