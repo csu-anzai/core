4f68eea6188cb0668da8f6cc50e72645ad65f8c7
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-04-08 17:58:26 +0200

    feat(system): SystemChangelogRestorerTest call also getter to check the fitting method return type

--- module_system/tests/SystemChangelogRestorerTest.php
+++ module_system/tests/SystemChangelogRestorerTest.php
@@ -4,6 +4,7 @@ namespace Kajona\System\Tests;
 
 use Kajona\System\System\Classloader;
 use Kajona\System\System\Date;
+use Kajona\System\System\Reflection;
 use Kajona\System\System\Resourceloader;
 use Kajona\System\System\Root;
 use Kajona\System\System\SystemChangelogRestorer;
@@ -22,11 +23,21 @@ class SystemChangelogRestorerTest extends Testbase
      */
     public function testRestore($model)
     {
+        // restore which calls all setter with null since we have no past values
         try {
             $restorer = new SystemChangelogRestorer();
             $restorer->restoreObject($model, new Date());
         } catch (\InvalidArgumentException $e) {
             // the AuswertungConfig::setObjEnddate throws an exception if the restorer tries to set null as value
+        }
+
+        // now call all getter which also checks whether each getter has a fitting return type
+        $reflection = new Reflection($model);
+        $properties = $reflection->getPropertiesWithAnnotation(SystemChangelogRestorer::ANNOTATION_PROPERTY_VERSIONABLE);
+
+        foreach ($properties as $property => $annotation) {
+            $method = $reflection->getGetter($property);
+            $model->$method();
         }
 
         $this->assertInstanceOf(VersionableInterface::class, $model);
