06f50d4c23c11d21bba82106794090ac03f90a7c
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-05-13 13:25:58 +0200

    feat(webpack) : started documentation for the new build process

--- _buildfiles/pathsFinder.js
+++ _buildfiles/pathsFinder.js
@@ -1,86 +1,103 @@
-const globby = require("globby");
-const glob = require("glob");
-const fs = require("fs");
-
+const globby = require('globby')
+const glob = require('glob')
+const fs = require('fs')
 
 module.exports = {
-
     getTsPaths: async () => {
-        let modulesPaths = null;
-        if (!fs.existsSync("./../../project/packageconfig.json")) {
-            //fallback: the complete ts file list
-            return glob.sync("../../core*/module_*/scripts/**/*.ts");
+        let modulesPaths = null
+        if (!fs.existsSync('./../../project/packageconfig.json')) {
+            // fallback: the complete ts file list
+            return glob.sync('../../core*/module_*/scripts/**/*.ts')
         }
-        let packageConfig = require("./../../project/packageconfig.json");
+        let packageConfig = require('./../../project/packageconfig.json')
 
         try {
-            let moduleMap = [];
+            let moduleMap = []
             for (let coreEntry in packageConfig) {
                 for (let i = 0; i < packageConfig[coreEntry].length; i++) {
                     moduleMap.push(
-                        "../../" + coreEntry + "/" + packageConfig[coreEntry][i] + "/scripts/**/*.ts"
+                        '../../' +
+                            coreEntry +
+                            '/' +
+                            packageConfig[coreEntry][i] +
+                            '/scripts/**/*.ts'
                     )
                 }
             }
-            modulesPaths = await globby(moduleMap);
-            //console.log("included ts files : ", modulesPaths);
-            return modulesPaths;
+            modulesPaths = await globby(moduleMap)
+            // console.log("included ts files : ", modulesPaths);
+            return modulesPaths
         } catch (e) {
-            console.log(e);
+            console.log(e)
         }
     },
 
     getLessPaths: async () => {
-        let lessPaths = null;
-        if (!fs.existsSync("./../../project/packageconfig.json")) {
-            //fallback: an empty file to avoid compiler errors
-            await fs.writeFile(__dirname+'/../module_v4skin/admin/skins/kajona_v4/less/styles.less', '', function(er) {
-                if (er !== null) {
-                    console.log(er);
+        let lessPaths = null
+        if (!fs.existsSync('./../../project/packageconfig.json')) {
+            // fallback: an empty file to avoid compiler errors
+            await fs.writeFile(
+                __dirname +
+                    '/../module_v4skin/admin/skins/kajona_v4/less/styles.less',
+                '',
+                function (er) {
+                    if (er !== null) {
+                        console.log(er)
+                    }
                 }
-            });
-            return [];
+            )
+            return []
         }
-        let packageConfig = require("./../../project/packageconfig.json");
+        let packageConfig = require('./../../project/packageconfig.json')
 
         try {
-            let moduleMap = [];
-            moduleMap.push("../../core/module_v4skin/admin/skins/kajona_v4/less/bootstrap.less");
+            let moduleMap = []
+            moduleMap.push(
+                '../../core/module_v4skin/admin/skins/kajona_v4/less/bootstrap.less'
+            )
 
             for (let coreEntry in packageConfig) {
                 for (let i = 0; i < packageConfig[coreEntry].length; i++) {
-
-                    if (packageConfig[coreEntry][i] === "_buildfiles") {
-                        continue;
+                    if (packageConfig[coreEntry][i] === '_buildfiles') {
+                        continue
                     }
 
-                    if (packageConfig[coreEntry][i] === "module_v4skin") {
-                        continue;
+                    if (packageConfig[coreEntry][i] === 'module_v4skin') {
+                        continue
                     }
 
                     moduleMap.push(
-                        "../../" + coreEntry + "/" + packageConfig[coreEntry][i] + "/**/less/**/*.less"
+                        '../../' +
+                            coreEntry +
+                            '/' +
+                            packageConfig[coreEntry][i] +
+                            '/**/less/**/*.less'
                     )
                 }
             }
-            moduleMap.push("../../project/**/less/*.less");
+            moduleMap.push('../../project/**/less/*.less')
 
-            lessPaths = await globby(moduleMap);
+            lessPaths = await globby(moduleMap)
 
-            let file = ' /* auto generated, do not change */\n';
+            let file = ' /* auto generated, do not change */\n'
             for (let i = 0; i < lessPaths.length; i++) {
-                file += ' @import "../../../../'+lessPaths[i]+'";\n';
+                file += ' @import "../../../../' + lessPaths[i] + '";\n'
             }
 
-            await fs.writeFile(__dirname+'/../module_v4skin/admin/skins/kajona_v4/less/styles.less', file, function(er) {
-                if (er !== null) {
-                    console.log(er);
+            await fs.writeFile(
+                __dirname +
+                    '/../module_v4skin/admin/skins/kajona_v4/less/styles.less',
+                file,
+                function (er) {
+                    if (er !== null) {
+                        console.log(er)
+                    }
                 }
-            });
+            )
 
-            return lessPaths;
+            return lessPaths
         } catch (e) {
-            console.log(e);
+            console.log(e)
         }
     }
-};
+}
--- _buildfiles/webpack.config.js
+++ _buildfiles/webpack.config.js
@@ -1,181 +1,174 @@
-const path = require("path");
-const webpack = require("webpack");
-const glob = require("glob");
-const Dotenv = require("dotenv-webpack");
-const pathsFinder = require("./pathsFinder");
-const LiveReloadPlugin = require("webpack-livereload-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
-const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { VueLoaderPlugin } = require("vue-loader");
+const path = require('path')
+const webpack = require('webpack')
+const glob = require('glob')
+const Dotenv = require('dotenv-webpack')
+const pathsFinder = require('./pathsFinder')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
+const { VueLoaderPlugin } = require('vue-loader')
 const liveReloadOptions = {
-  hostname: "localhost",
-  protocol: "http"
-};
+    hostname: 'localhost',
+    protocol: 'http'
+}
 
 module.exports = async env => {
-  const devMode = env.NODE_ENV !== "production";
-  console.log("Build Type : ", env.NODE_ENV);
-  let tsPaths = await pathsFinder.getTsPaths();
-  let lessPaths = await pathsFinder.getLessPaths();
+    const devMode = env.NODE_ENV !== 'production'
+    console.log('Build Type : ', env.NODE_ENV)
+    let tsPaths = await pathsFinder.getTsPaths() // returns the paths of all the modules found in packageconfig.json
+    let lessPaths = await pathsFinder.getLessPaths()
 
-  return {
-    entry: {
-      agp: devMode ? glob.sync("../../core*/module_*/scripts/**/*.ts") : tsPaths
-    },
-    output: {
-      filename: "./[name].min.js",
-      path: path.resolve(__dirname, "../module_system/scripts/")
-    },
+    return {
+        entry: {
+            agp: devMode // if dev mode compile all the modules
+                ? glob.sync('../../core*/module_*/scripts/**/*.ts')
+                : tsPaths // else only compile the needed modules for prod
+        },
+        output: {
+            filename: './[name].min.js',
+            path: path.resolve(__dirname, '../module_system/scripts/')
+        },
 
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/, // vue loader for the template files
 
-          loader: "vue-loader",
-          options: {
-            loaders: {
-              scss: "vue-style-loader!css-loader!sass-loader",
-              sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-            }
-          }
-        },
-        {
-          test: /\.tsx?$/,
-          use: [
-            { loader: "babel-loader" },
-            {
-              loader: "ts-loader",
-              options: {
-                appendTsSuffixTo: [/\.vue$/],
-                configFile: path.resolve(__dirname, "./tsconfig.json")
-              }
-            }
-          ],
-
-          exclude: /node_modules/
-        },
-        {
-          test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.(png|jpg|gif)$/,
-          loader: "url-loader"
-        },
-        {
-          test: /\.less$/,
-          use: [
-            {
-              // loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader // creates style nodes from JS strings
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader" // translates CSS into CommonJS
-            },
-            {
-              loader: "less-loader" // compiles Less to CSS
-            }
-            // {
-            //   loader: MiniCssExtractPlugin.loader,
-            //   options: {
-            //     // you can specify a publicPath here
-            //     // by default it use publicPath in webpackOptions.output
-            //     // publicPath: '../'
-            //   }
-            // }
-          ]
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }
-      ]
-    },
-    resolve: {
-      modules: [path.resolve(__dirname, "./node_modules")],
-      extensions: [".ts", ".js", ".vue", ".json", ".less"],
-      alias: {
-        vue$: "vue/dist/vue.esm.js",
-        "load-image": "blueimp-load-image/js/load-image.js",
-        "load-image-meta": "blueimp-load-image/js/load-image-meta.js",
-        "load-image-exif": "blueimp-load-image/js/load-image-exif.js",
-        "load-image-scale": "blueimp-load-image/js/load-image-scale.js",
-        "canvas-to-blob": "blueimp-canvas-to-blob/js/canvas-to-blob.js",
-        "jquery-ui/ui/widget": "blueimp-file-upload/js/vendor/jquery.ui.widget.js",
-        "@": path.resolve(__dirname, "../../"),
-        core: path.resolve(__dirname, "../"),
-        core_agp: path.resolve(__dirname, "../../core_agp")
-      }
-    },
-    plugins: devMode
-      ? [
-          new Dotenv({
-            path: devMode
-              ? path.resolve(__dirname, ".env.dev")
-              : path.resolve(__dirname, ".env.prod")
-          }),
-          new webpack.ProvidePlugin({
-            jQuery: "jquery",
-            $: "jquery",
-            jquery: "jquery"
-          }),
-          new LiveReloadPlugin(liveReloadOptions),
-          // new webpack.HotModuleReplacementPlugin()
-          new WatchMissingNodeModulesPlugin(path.resolve("node_modules")),
-          new VueLoaderPlugin()
-        ]
-      : [
-          new Dotenv({
-            path: devMode
-              ? path.resolve(__dirname, ".env.dev")
-              : path.resolve(__dirname, ".env.prod")
-          }),
-          new webpack.ProvidePlugin({
-            jQuery: "jquery",
-            $: "jquery",
-            jquery: "jquery"
-          }),
-          // new CompressionPlugin(),
-          // new MiniCssExtractPlugin({
-          //   filename: "styles.min.css",
-          //   chunkFilename: "styles.min.chunk.css"
-          // })
-          new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-          new VueLoaderPlugin()
-        ],
-    // watch: true
-    optimization: {
-      minimize: !devMode,
-      minimizer: !devMode
-        ? [
-            new TerserPlugin({
-              terserOptions: {
-                parse: {
-                  ecma: 8
+                    loader: 'vue-loader',
+                    options: {
+                        loaders: {
+                            scss: 'vue-style-loader!css-loader!sass-loader',
+                            sass:
+                                'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+                        }
+                    }
                 },
-                compress: {
-                  ecma: 5,
-                  warnings: false,
-                  comparisons: false,
-                  inline: 2
+                {
+                    test: /\.tsx?$/, // typescript loader for the .ts and .tsx files
+                    use: [
+                        { loader: 'babel-loader' },
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                appendTsSuffixTo: [/\.vue$/], // needed to import vue's template files in .ts files
+                                configFile: path.resolve(
+                                    // path to the tsconfig.json file
+                                    __dirname,
+                                    './tsconfig.json'
+                                )
+                            }
+                        }
+                    ],
+
+                    exclude: /node_modules/
                 },
-                mangle: {
-                  safari10: true
+                {
+                    test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.(png|jpg|gif)$/, // loader for the fonts/images , makes import of this files possible
+                    loader: 'url-loader'
                 },
-                output: {
-                  ecma: 5,
-                  comments: false,
-                  ascii_only: true
+                {
+                    test: /\.less$/,
+                    use: [
+                        {
+                            loader: 'style-loader' // creates style nodes from JS strings
+                        },
+                        {
+                            loader: 'css-loader' // translates CSS into CommonJS
+                        },
+                        {
+                            loader: 'less-loader' // compiles Less to CSS
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/, // normal css loader
+                    use: ['style-loader', 'css-loader']
                 }
-              },
-              parallel: true,
-              cache: true
-            })
-          ]
-        : [],
-      splitChunks: {
-        chunks: "all",
-        name: "vendors.chunks"
-      }
+            ]
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, './node_modules')],
+            extensions: ['.ts', '.js', '.vue', '.json', '.less'],
+            alias: {
+                vue$: 'vue/dist/vue.esm.js',
+                'load-image': 'blueimp-load-image/js/load-image.js',
+                'load-image-meta': 'blueimp-load-image/js/load-image-meta.js',
+                'load-image-exif': 'blueimp-load-image/js/load-image-exif.js',
+                'load-image-scale': 'blueimp-load-image/js/load-image-scale.js',
+                'canvas-to-blob': 'blueimp-canvas-to-blob/js/canvas-to-blob.js',
+                'jquery-ui/ui/widget':
+                    'blueimp-file-upload/js/vendor/jquery.ui.widget.js',
+                '@': path.resolve(__dirname, '../../'),
+                core: path.resolve(__dirname, '../'),
+                core_agp: path.resolve(__dirname, '../../core_agp')
+            }
+        },
+        plugins: devMode
+            ? [
+                new Dotenv({
+                    path: devMode
+                        ? path.resolve(__dirname, '.env.dev')
+                        : path.resolve(__dirname, '.env.prod')
+                }),
+                new webpack.ProvidePlugin({
+                    jQuery: 'jquery',
+                    $: 'jquery',
+                    jquery: 'jquery'
+                }),
+                new LiveReloadPlugin(liveReloadOptions),
+                new WatchMissingNodeModulesPlugin(
+                    path.resolve('node_modules')
+                ),
+                new VueLoaderPlugin()
+            ]
+            : [
+                new Dotenv({
+                    path: devMode
+                        ? path.resolve(__dirname, '.env.dev')
+                        : path.resolve(__dirname, '.env.prod')
+                }),
+                new webpack.ProvidePlugin({
+                    jQuery: 'jquery',
+                    $: 'jquery',
+                    jquery: 'jquery'
+                }),
+                new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+                new VueLoaderPlugin()
+            ],
+        // watch: true
+        optimization: {
+            minimize: !devMode,
+            minimizer: !devMode
+                ? [
+                    new TerserPlugin({
+                        terserOptions: {
+                            parse: {
+                                ecma: 8
+                            },
+                            compress: {
+                                ecma: 5,
+                                warnings: false,
+                                comparisons: false,
+                                inline: 2
+                            },
+                            mangle: {
+                                safari10: true
+                            },
+                            output: {
+                                ecma: 5,
+                                comments: false,
+                                ascii_only: true
+                            }
+                        },
+                        parallel: true,
+                        cache: true
+                    })
+                ]
+                : [],
+            splitChunks: {
+                chunks: 'all',
+                name: 'vendors.chunks'
+            }
+        }
     }
-  };
-};
+}
