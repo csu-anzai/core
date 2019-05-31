10dd4cc12185b9738d063cbd7d97b6579b83194f
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-05-31 16:12:29 +0200

    feat(system) : change i18n locale on user's language change

--- module_system/scripts/kajona/VueMainComponent/VueMain.ts
+++ module_system/scripts/kajona/VueMainComponent/VueMain.ts
@@ -1,7 +1,11 @@
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 import Searchbar from 'core/module_search/scripts/components/Searchbar/Searchbar.vue'
 @Component({ components: { Searchbar } })
 class VueMain extends Vue {
-
+private language : String = KAJONA_LANGUAGE
+mounted () {
+    // register i18n globally to change the languages from outside Vue
+    (<any>window).i18n = this.$i18n
+}
 }
 export default VueMain
