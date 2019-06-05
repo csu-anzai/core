07d8d47c4ad9ee24cb722dca733ab28fa000ff6a
Difo0505 <dhafer.harrathi@artemeon.de>
2019-06-05 02:57:59 +0200

    feat(*) : added bootstrap-vue for future bootstrap components use

--- module_system/scripts/kajona/VueMainComponent/VueMain.ts
+++ module_system/scripts/kajona/VueMainComponent/VueMain.ts
@@ -1,11 +1,14 @@
 import { Vue, Component, Watch } from 'vue-property-decorator'
 import Searchbar from 'core/module_search/scripts/components/Searchbar/Searchbar.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 @Component({ components: { Searchbar } })
 class VueMain extends Vue {
-private language : String = KAJONA_LANGUAGE
-mounted () {
+    mounted () {
     // register i18n globally to change the languages from outside Vue
-    (<any>window).i18n = this.$i18n
-}
+        (<any>window).i18n = this.$i18n
+    }
 }
 export default VueMain
