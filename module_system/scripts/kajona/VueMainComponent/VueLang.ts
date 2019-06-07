c9a224cd8db9944e6ccd59c74bfb31d33a1a5629
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-06-07 13:40:52 +0200

    feat(system) : added i18n lang support for module_system

--- module_system/scripts/kajona/VueMainComponent/VueLang.ts
+++ module_system/scripts/kajona/VueMainComponent/VueLang.ts
@@ -4,16 +4,20 @@ import SearchDe from 'core/module_search/scripts/Lang/lang_search_de'
 import SearchEn from 'core/module_search/scripts/Lang/lang_search_en'
 import DashboardDe from 'core/module_dashboard/scripts/Lang/lang_dashboard_de'
 import DashboardEn from 'core/module_dashboard/scripts/Lang/lang_dashboard_en'
+import SystemDe from 'core/module_system/scripts/Lang/lang_system_de'
+import SystemEn from 'core/module_system/scripts/Lang/lang_system_en'
 Vue.use(VueI18n)
 const locale = KAJONA_LANGUAGE
 const messages = {
     de: {
         search: SearchDe,
-        dashboard: DashboardDe
+        dashboard: DashboardDe,
+        system: SystemDe
     },
     en: {
         search: SearchEn,
-        dashboard: DashboardEn
+        dashboard: DashboardEn,
+        system: SystemEn
     }
 }
 export default new VueI18n({
