647895684d752b6570527eb47eacd0b65e3a4f9e
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-06-07 11:04:37 +0200

    fix(system) : moved Loader component to Loader folder

---
+++ module_system/scripts/components/Loader/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div v-bind:class="{loadingContainer : loading}"></div>
+</template>
+<script lang="ts" src="./Loader.ts">
+</script>
+<style lang="less" scoped src="./Loader.less">
+</style>
