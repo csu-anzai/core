1caeb9494a3ff7a5ad066e0d2f16037c1a6e7c0f
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-06-17 11:09:58 +0200

    feat(system) : added vue-multiselect default styling

--- module_system/scripts/components/Multiselect/Multiselect.ts
+++ module_system/scripts/components/Multiselect/Multiselect.ts
@@ -1,6 +1,7 @@
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 import uuid from 'uuid/v1'
 import MSelect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
 @Component({ components: { MSelect } }) class Multiselect extends Vue {
 @Prop({ type: Array, required: true }) options : Array<any>
 @Prop({ type: String, required: true }) label : string
