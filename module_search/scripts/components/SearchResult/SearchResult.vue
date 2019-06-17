87474783a06c03b268958dfaaf34fe3f1d05e055
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-06-17 16:08:55 +0200

    feat(search) : changed on click behaviour + added missing searchResult properties + added styling

--- module_search/scripts/components/SearchResult/SearchResult.vue
+++ module_search/scripts/components/SearchResult/SearchResult.vue
@@ -3,13 +3,20 @@
     <div
       v-for="item in searchResults"
       :key="item.systemid"
-      class="searchResultContainer"
+      class="searchResultContainer row"
+      @click="close(item.link)"
     >
-      <a :href="item.link" @click="close">
-        <span v-html="item.icon"></span>
-        {{item.description}}
-      </a>
-      <small>{{item.module}}</small>
+      <div class="col-sm-2">
+        <small>{{$t("search.module_label")}} : {{item.module}}</small>
+      </div>
+      <div class="col-sm-10">
+        <p>
+          <span v-html="item.icon"></span>
+          {{item.description}}
+        </p>
+
+        <small>{{$t("search.last_modification_date")}} {{item.lastModifiedTime}} {{$t("search.last_modification_user")}} {{item.lastModifiedBy}}</small>
+      </div>
     </div>
   </div>
 </template>
