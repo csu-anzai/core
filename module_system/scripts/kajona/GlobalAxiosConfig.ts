1b1303fa619efed095aff04525440ecb9a3cf556
Difo0505 <dhafer.harrathi@artemeon.de>
2019-06-21 10:30:48 +0200

    feat(system) : wrapper class for axios : added global paramSerializer + set baseURL globally + added middleware for loading animation

---
+++ module_system/scripts/kajona/GlobalAxiosConfig.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import qs from 'qs'
+import { Service } from 'axios-middleware'
+import WorkingIndicator from './WorkingIndicator'
+/**
+ * a wrapper class for axios used to configure axios globally and adds middleware for loading animation
+ */
+class GlobalAxiosConfig {
+    private service : any
+    constructor () {
+        // global parameter serializer for axios : converts json data to url params
+        axios.defaults.paramsSerializer = (params) => {
+            return qs.stringify(params, { arrayFormat: 'brackets' })
+        }
+        // golbal axios's baseURL config
+        axios.defaults.baseURL = KAJONA_WEBPATH
+        this.createMiddleware()
+    }
+    /**
+     * use a middleware to be able to start/stop loader animation onRequest
+     */
+    private createMiddleware () : void {
+        console.log('create middleware')
+        this.service = new Service(axios)
+        this.service.register({
+            onRequest (config : any) {
+                WorkingIndicator.start()
+                return config
+            },
+            onResponse (response : any) {
+                WorkingIndicator.stop()
+                return response
+            }
+        })
+    }
+}
+
+export default GlobalAxiosConfig
