1dc9b57837d196be6e29e409c5777ecf5e34c58f
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-06-25 13:44:52 +0200

    feat(api): ApiUserLoginListener add listener which generates an access token on login

---
+++ module_api/event/ApiUserLoginListener.php
@@ -0,0 +1,65 @@
+<?php
+/*"******************************************************************************************************
+ *   (c) ARTEMEON Management Partner GmbH
+ *       Published under the GNU LGPL v2.1
+ ********************************************************************************************************/
+
+namespace Kajona\Api\Event;
+
+use Firebase\JWT\JWT;
+use Kajona\Api\System\ServiceProvider;
+use Kajona\Api\System\TokenReader;
+use Kajona\System\System\Carrier;
+use Kajona\System\System\CoreEventdispatcher;
+use Kajona\System\System\GenericeventListenerInterface;
+use Kajona\System\System\Lifecycle\ServiceLifeCycleFactory;
+use Kajona\System\System\Objectfactory;
+use Kajona\System\System\SystemEventidentifier;
+use Kajona\System\System\UserUser;
+
+/**
+ * @package module_api
+ * @author christoph.kappestein@artemeon.de
+ */
+class ApiUserLoginListener implements GenericeventListenerInterface
+{
+    /**
+     * @param string $strEventName
+     * @param array $arrArguments
+     * @return bool
+     */
+    public function handleEvent($strEventName, array $arrArguments)
+    {
+        list($strUserid) = $arrArguments;
+
+        /** @var TokenReader $tokenReader */
+        $tokenReader = Carrier::getInstance()->getContainer()->offsetGet(ServiceProvider::STR_TOKEN_READER);
+
+        $user = Objectfactory::getInstance()->getObject($strUserid);
+
+        if ($user instanceof UserUser) {
+            // every time the user executes a login we generate a new access token
+            $payload = [
+                "uid" => $user->getSystemid(),
+                "exp" => time() + (60 * 60), // by default the token is valid for one hour
+            ];
+
+            $token = JWT::encode($payload, $tokenReader->getToken());
+
+            $user->setStrAccessToken($token);
+            ServiceLifeCycleFactory::getLifeCycle(get_class($user))->update($user);
+        }
+
+        return true;
+    }
+
+    /**
+     * @return void
+     */
+    public static function staticConstruct()
+    {
+        CoreEventdispatcher::getInstance()->removeAndAddListener(SystemEventidentifier::EVENT_SYSTEM_USERLOGIN, new ApiUserLoginListener());
+    }
+}
+
+ApiUserLoginListener::staticConstruct();
