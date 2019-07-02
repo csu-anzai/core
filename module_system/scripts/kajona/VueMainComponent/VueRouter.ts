a1008dd92164644c383da10dc1ade021bd9ced65
Difo0505 <dhafer.harrathi@artemeon.de>
2019-07-02 03:01:46 +0200

    feat(*) : started working on reportgenerator's minimap slider

--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Reportgenerator from 'core_agp/module_reportconfigurator/scripts/components/Reportgenerator.vue'
 Vue.use(<any>Router)
 
 export default new Router({
-    mode: 'history',
-    routes: []
+    routes: [
+        { path: '/reportconfigurator/test',
+            component: Reportgenerator
+        }
+    ]
 })
