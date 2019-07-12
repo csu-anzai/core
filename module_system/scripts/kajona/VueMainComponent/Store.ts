644ee6ef06dd2f60bc94900a56c2463535679ca6
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-07-12 13:21:25 +0200

    fix(*) : merged axios class and removed unnecessary files

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import SearchModule from 'core/module_search/scripts/modules/SearchModule'
-// import VueI18n from 'vue-i18n'
+import ReportconfiguratorModule from 'core_agp/module_reportconfigurator/scripts/modules/ReportconfiguratorModule'
 Vue.use(<any>Vuex)
-// Vue.use(VueI18n)
+
 export default new Vuex.Store({
     modules: {
-        SearchModule: SearchModule
+        ReportconfiguratorModule: ReportconfiguratorModule
     }
 })
