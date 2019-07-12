4e28dce44ca25329350e0f3eccb8987a77728003
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-07-12 13:24:07 +0200

    fix(system) : deleted files from last merge

--- module_system/scripts/components/Loader/Loader.ts
+++
@@ -1,5 +0,0 @@
-import { Component, Vue, Prop } from 'vue-property-decorator'
-@Component class Loader extends Vue {
-@Prop(Boolean) loading : boolean
-}
-export default Loader
--- module_system/scripts/components/Loader/Loader.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div v-bind:class="{loadingContainer : loading}"></div>
-</template>
-<script lang="ts" src="./Loader.ts">
-</script>
-
