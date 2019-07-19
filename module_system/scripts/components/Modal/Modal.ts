ec784467515807c674bcf494ef946578c1968a97
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-07-19 17:12:32 +0200

    feat(*) : added all the global Components to this branch to use in other branches

---
+++ module_system/scripts/components/Modal/Modal.ts
@@ -0,0 +1,28 @@
+import $ from 'jquery'
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+import uuid from 'uuid/v1'
+@Component class Modal extends Vue {
+@Prop(Boolean) show : boolean = false
+private modalId : string = uuid()
+
+private mounted () : void{
+    $('#' + this.modalId).on('hidden.bs.modal', this.onClose)
+    $('#' + this.modalId).on('shown.bs.modal', () => {
+        this.$emit('open')
+    })
+}
+
+private onClose (e) :void{
+    this.$emit('close')
+}
+
+@Watch('show') onchange () : void{
+    if (this.show) {
+        $('#' + this.modalId).modal('show')
+    } else {
+        $('#' + this.modalId).modal('hide')
+    }
+}
+}
+
+export default Modal
