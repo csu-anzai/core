ded1ba1b34a25de2c68b6076a2b5989a07f313a7
Dhafer <soprano1993@live.fr>
2019-07-30 19:10:47 +0200

    fix(system) : fixed merging errors

--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -3,17 +3,23 @@ import Router from 'vue-router'
 import Reportgenerator from 'core_agp/module_reportconfigurator/scripts/components/Reportgenerator/Reportgenerator.vue'
 Vue.use(<any>Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         { path: '/vm/reportconfigurator/:reportId',
-            component: Reportgenerator
+            component: Reportgenerator,
+            beforeEnter: resetContainer
         }
     ]
-
+})
 router.beforeEach((to, from, next) => {
     let moduleOutput = document.getElementById('moduleOutput')
     moduleOutput.innerHTML = ''
     next()
 })
+function resetContainer (to, from, next) : void {
+    let moduleOutput = document.getElementById('moduleOutput')
+    moduleOutput.innerHTML = ''
+    next()
+}
 
 export default router
