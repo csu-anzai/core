807c3cc77bc71ca93fa1f7d581e5ebb1695b1118
Dhafer <soprano1993@live.fr>
2019-07-31 03:33:15 +0200

    feat(system) : reset ContentToolbar and BreadCrumb before entering a defined vue-route

--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ContentToolbar from 'core/module_system/scripts/kajona/ContentToolbar'
+import BreadCrumb from 'core/module_system/scripts/kajona/Breadcrumb'
 Vue.use(<any>Router)
 
 const router = new Router({
     routes: []
 })
 function resetContainer (to, from, next) : void {
+    ContentToolbar.resetBar()
+    BreadCrumb.resetBar()
     let moduleOutput = document.getElementById('moduleOutput')
     moduleOutput.innerHTML = ''
     next()
