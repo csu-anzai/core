899865299b429047b7addf81f36bd542af280e5e
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-08-01 09:22:46 +0200

    # Conflicts: #	_buildfiles/package-lock.json #	_buildfiles/package.json #	module_system/scripts/components/Modal/Modal.ts #	module_system/scripts/kajona/App.ts
    # Conflicts:
    #	_buildfiles/package-lock.json

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import FailCodeModule from 'core_customer/module_hsbcact/scripts/modules/FailCodeModule'
 Vue.use(<any>Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {}
+    // state: {},
+    // mutations: {},
+    // actions: {},
+    // getters: {},
+    modules: { failCode: FailCodeModule }
 })
