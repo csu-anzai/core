7ef7075328438b71cdb6576e35d935520bea12cb
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-08-02 17:23:09 +0200

    feat(system) : modified vualang.ts and app.ts to work with the new decorator

--- module_system/scripts/kajona/VueMainComponent/VueLang.ts
+++ module_system/scripts/kajona/VueMainComponent/VueLang.ts
@@ -1,20 +1,8 @@
 import Vue from 'vue'
-import VueI18n, { LocaleMessageObject } from 'vue-i18n'
-import Lang from 'core/module_system/scripts/kajona/Lang'
+import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
-async function getLanguages () {
-    const de : LocaleMessageObject = {
-
-    }
-    const en : LocaleMessageObject = {
-
-    }
-
-    return new VueI18n({
-        locale: KAJONA_LANGUAGE,
-        messages: { de, en }
-    })
-}
-
-export default getLanguages()
+export default new VueI18n({
+    locale: KAJONA_LANGUAGE,
+    messages: { en: {}, de: {} }
+})
