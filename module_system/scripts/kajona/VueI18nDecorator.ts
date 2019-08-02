6dc4a62ae67e3c92d440bba43f1f7d6f390abe64
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-08-02 17:15:50 +0200

    feat(system) : added FetchLang decorator to implement in vue components in order to fetch the wished lang modules

---
+++ module_system/scripts/kajona/VueI18nDecorator.ts
@@ -0,0 +1,17 @@
+import { createDecorator } from 'vue-class-component'
+import Lang from 'core/module_system/scripts/kajona/Lang'
+export const FetchLang = (modules : Array<string>) => {
+    return createDecorator((component, key) => {
+        component.created = async () => {
+            let en = {}
+            let de = {}
+            await Promise.all(modules.map(async module => {
+                en[module] = await Lang.fetchModule(module, 'en')
+                de[module] = await Lang.fetchModule(module, 'de')
+            }))
+            console.log('en : ', en)
+            window.i18n.setLocaleMessage('en', en)
+            window.i18n.setLocaleMessage('de', de)
+        }
+    })
+}
