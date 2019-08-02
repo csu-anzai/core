a10ba9688fdf18b73d248c6417bc89c56816142a
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-08-02 16:17:52 +0200

    Revert "# Conflicts: #	_buildfiles/package-lock.json #	_buildfiles/package.json #	module_system/scripts/components/Modal/Modal.ts #	module_system/scripts/kajona/App.ts"
    This reverts commit 89986529

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import FailCodeModule from 'core_customer/module_hsbcact/scripts/modules/FailCodeModule'
+
 Vue.use(<any>Vuex)
 
 export default new Vuex.Store({
-    // state: {},
-    // mutations: {},
-    // actions: {},
-    // getters: {},
-    modules: { failCode: FailCodeModule }
+    state: {},
+    mutations: {},
+    actions: {},
+    getters: {}
 })
--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -1,23 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import FailCode from 'core_customer/module_hsbcact/scripts/components/FailCodeWrapper/FailCodeWrapper.vue'
-import store from './Store'
+
 Vue.use(<any>Router)
 
 export default new Router({
-    routes: [ {
-        path: '/vm/hsbcact/failCode',
-        component: FailCode,
-        props: route => ({
-            query: {
-                objects: route.query.objects
-            }
-        }),
-        beforeEnter: (to, from, next) => {
-            let objects = to.query.objects.toString()
-            let str = objects.split(',')
-            store.commit('failCode/SET_OBJECTS', str)
-            next()
-        }
-    }]
+    routes: []
 })
