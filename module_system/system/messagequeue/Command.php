b4de4c880c188c7f3702551fd123f2b3e662dee1
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-08-05 11:09:36 +0200

    docs(system): Command update doc

--- module_system/system/messagequeue/Command.php
+++ module_system/system/messagequeue/Command.php
@@ -15,14 +15,14 @@ namespace Kajona\System\System\Messagequeue;
 abstract class Command
 {
     /**
-     * Returns the event data to a simple associative array which can be encoded by json_encode
+     * Returns the command data to a simple associative array which can be encoded by json_encode
      *
      * @return array
      */
     abstract public function toArray() : array;
 
     /**
-     * Returns the event object based on the array structure which was previously returns by the toArray method
+     * Returns the command object based on the array structure which was previously returns by the toArray method
      *
      * @param array $data
      * @return static
