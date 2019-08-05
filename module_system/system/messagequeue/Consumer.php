3ab0860c8511219708110d2290a192128e28f22e
Christoph Kappestein <christoph.kappestein@artemeon.de>
2019-08-05 11:19:24 +0200

    feat(system): split consume and consumeAll method

--- module_system/system/messagequeue/Consumer.php
+++ module_system/system/messagequeue/Consumer.php
@@ -56,44 +56,53 @@ class Consumer
         $this->logger = $logger;
     }
 
-    public function consume(): void
+    public function consumeAll(): void
     {
         $startTime = time();
         $result = $this->connection->getPArray('SELECT command_id, command_class, command_payload FROM agp_system_commands', [], 0, self::MAX_PREFETCH);
 
         foreach ($result as $row) {
-            $class = $row['command_class'];
-            $payload = \json_decode($row['command_payload'], true);
-
             // directly delete the event since otherwise this would block the queue in case the event throws an
             // unrecoverable error
             $this->connection->delete('agp_system_commands', ['command_id' => $row['command_id']]);
 
-            // check whether the event class exists
-            if (!class_exists($class)) {
-                continue;
-            }
+            $class = $row['command_class'];
+            $payload = \json_decode($row['command_payload'], true);
 
-            try {
-                if (!is_callable($class . '::fromArray', false, $callable)) {
-                    throw new \RuntimeException('Command has no fromArray method');
-                }
-
-                $command = call_user_func_array($callable, [$payload]);
-
-                if ($command instanceof Command) {
-                    $this->handleCommand($command);
-                } else {
-                    throw new \RuntimeException('Could not create command');
-                }
-            } catch (\Throwable $e) {
-                $this->logger->error($e->getMessage());
-            }
+            $this->consume($class, $payload);
 
             // in case this workflow runs too long break and wait for the next execution
             if (time() - $startTime > self::MAX_EXECUTION) {
                 break;
             }
+        }
+    }
+
+    /**
+     * @param string $class
+     * @param array $payload
+     */
+    private function consume(string $class, array $payload): void
+    {
+        // check whether the event class exists
+        if (!class_exists($class)) {
+            return;
+        }
+
+        try {
+            if (!is_callable($class . '::fromArray', false, $callable)) {
+                throw new \RuntimeException('Command has no fromArray method');
+            }
+
+            $command = call_user_func_array($callable, [$payload]);
+
+            if ($command instanceof Command) {
+                $this->handleCommand($command);
+            } else {
+                throw new \RuntimeException('Could not create command');
+            }
+        } catch (\Throwable $e) {
+            $this->logger->error($e->getMessage());
         }
     }
 
