f55f84fbc7bd39b4bdba0933657b717e93809016
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-08-16 15:56:52 +0200

    feat(system) : created a BreadCrumb vue component

---
+++ module_system/scripts/components/BreadCrumb/BreadCrumb.ts
@@ -0,0 +1,12 @@
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import BreadCrumbMaker from 'core/module_system/scripts/kajona/Breadcrumb'
+@Component class BreadCrumb extends Vue {
+    @Prop({ type: Array, required: true }) data !: Array<{link : string, title : string}>
+    private mounted () : void {
+        this.data.map(element => {
+            BreadCrumbMaker.appendLinkToPathNavigation("<a href='" + element.link + "'>" + element.title + '</a>')
+        })
+    }
+}
+
+export default BreadCrumb
