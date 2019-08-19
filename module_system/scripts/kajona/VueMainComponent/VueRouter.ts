8de9ef3ed570374579e19727d1c23792d32c3df6
rym.rjab <rym.rjab@artemeon.de>
2019-08-19 17:43:42 +0200

    feat(system): changed path because component is now under folder

--- module_system/scripts/kajona/VueMainComponent/VueRouter.ts
+++ module_system/scripts/kajona/VueMainComponent/VueRouter.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Router from '../Router'
-import RatingDetail from 'core_customer/module_hsbcact/scripts/components/RatingDetail.vue'
+import RatingDetail from 'core_customer/module_hsbcact/scripts/components/RatingDetail/RatingDetail.vue'
 Vue.use(<any>VueRouter)
 
 const router = new VueRouter({
