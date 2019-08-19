268234e80ca6d3446758c380c3433b6d898e79f6
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-08-19 16:31:34 +0200

    feat(system) : added pagination component

---
+++ module_system/scripts/components/Pagination/Pagination.ts
@@ -0,0 +1,12 @@
+import { Component, Mixins, Prop } from 'vue-property-decorator'
+import { BPagination } from 'bootstrap-vue'
+import { LangMixin } from 'core/module_system/scripts/kajona/VueMixings'
+@Component({ components: { BPagination } }) class Pagination extends Mixins(LangMixin(['commons'])) {
+@Prop({ type: Number, required: true }) total !: number
+
+private onChange (page : number) : void {
+    this.$emit('change', page)
+}
+}
+
+export default Pagination
---
+++ module_system/scripts/components/Pagination/Pagination.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="langFetched" class="pager">
+    <BPagination
+      :total-rows="total"
+      per-page="1"
+      :prev-text="$t('commons.commons_back')"
+      :next-text="$t('commons.commons_continue')"
+      @change="onChange($event)"
+    ></BPagination>
+  </div>
+</template>
+
+<script lang="ts" src="./Pagination.ts">
+</script>
+<style lang="less" scoped src="./Pagination.less">
+</style>
