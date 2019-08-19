ab3c35ed77ead19015edd3cc0b4df11f53490618
rym.rjab <rym.rjab@artemeon.de>
2019-08-19 17:06:13 +0200

    feat(system): added RatingDetail to router and store

--- module_system/scripts/kajona/VueMainComponent/Store.ts
+++ module_system/scripts/kajona/VueMainComponent/Store.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import RatingDetailModule from 'core_customer/module_hsbcact/scripts/modules/RatingDetailModule'
 
 Vue.use(<any>Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    getters: {}
+    modules: {
+        ratingModule: RatingDetailModule
+    }
 })
