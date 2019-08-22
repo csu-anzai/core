3aeec1ab2427413bc7cadc0bdbb3528a681ea16c
Dhafer <soprano1993@live.fr>
2019-08-22 02:55:38 +0200

    feat(system) : display current selected page in pagination

--- module_system/scripts/components/Pagination/Pagination.ts
+++ module_system/scripts/components/Pagination/Pagination.ts
@@ -1,8 +1,10 @@
 import { Component, Mixins, Prop } from 'vue-property-decorator'
 import { BPagination } from 'bootstrap-vue'
 import { LangMixin } from 'core/module_system/scripts/kajona/VueMixings'
+
 @Component({ components: { BPagination } }) class Pagination extends Mixins(LangMixin(['commons'])) {
 @Prop({ type: Number, required: true }) total !: number
+@Prop({ type: Number, required: true }) currentPage !: number
 
 private onChange (page : number) : void {
     this.$emit('change', page)
