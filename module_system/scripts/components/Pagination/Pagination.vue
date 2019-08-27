2f53461717ee6ef6f2738bba1e031d58ddef835b
Dhafer Harrathi <dhaferharrathi@artemeon.de>
2019-08-27 16:28:06 +0200

    feat(system) : created Pagination component instead of using the one from bootstrap-vue

--- module_system/scripts/components/Pagination/Pagination.vue
+++ module_system/scripts/components/Pagination/Pagination.vue
@@ -1,17 +1,29 @@
 <template>
   <div v-if="langFetched" class="pager">
-    <BPagination
-      :total-rows="total"
-      per-page="1"
-      :prev-text="$t('commons.commons_back')"
-      :next-text="$t('commons.commons_continue')"
-      v-model="currentPage"
-      @change="onChange($event)"
-    ></BPagination>
+    <ul class="pagination">
+      <li v-if="current!==1" @click="previous()">
+        <a>« {{$t('commons.commons_back')}}</a>
+      </li>
+      <li
+        v-for="(item, index) in total"
+        :key="index"
+        v-bind:class="{active : current === index+1}"
+        @click="changePage(index+1)"
+      >
+        <a v-bind:class="{active : current === index+1}">{{index+1}}</a>
+      </li>
+      <li @click="next()" v-if="current!==total">
+        <a>{{$t('commons.commons_continue')}} »</a>
+      </li>
+      <li>
+        <a>
+          {{$t("system.pageview_total")}}
+          {{total}}
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts" src="./Pagination.ts">
 </script>
-<style lang="less" scoped src="./Pagination.less">
-</style>
