daf9d602840dede876ca0ea0efc7e05d52e50a01
Stefan Idler <sidler@mulchprod.de>
2019-08-28 23:37:02 +0200

    feat(dashboard): updated fullcalendar to 4.0

--- module_dashboard/scripts/kajona/DashboardCalendar.ts
+++ module_dashboard/scripts/kajona/DashboardCalendar.ts
@@ -1,50 +1,53 @@
 import $ from 'jquery'
 import WorkingIndicator from '../../../module_system/scripts/kajona/WorkingIndicator'
 import Tooltip from '../../../module_system/scripts/kajona/Tooltip'
-import Ajax from "core/module_system/scripts/kajona/Ajax"
-import DialogHelper from "core/module_v4skin/scripts/kajona/DialogHelper"
-import Lang from "core/module_system/scripts/kajona/Lang"
+import Ajax from 'core/module_system/scripts/kajona/Ajax'
+import DialogHelper from 'core/module_v4skin/scripts/kajona/DialogHelper'
+import Lang from 'core/module_system/scripts/kajona/Lang'
+
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import listGridPlugin from '@fullcalendar/list'
+import deLocale from '@fullcalendar/core/locales/de'
+import enLocale from '@fullcalendar/core/locales/en-gb'
 
 class DashboardCalendar {
-    public static init() {
-        import(/* webpackChunkName: "fullcalendar" */ 'fullcalendar').then(({default: fullCalendar}) => {
-            $('#dashboard-calendar').fullCalendar({
-                header: {
-                    left: 'prev,next',
-                    center: 'title',
-                    right: ''
-                },
-                editable: false,
-                // theme: false,
-                locale: KAJONA_LANGUAGE,
-                eventLimit: true,
-                events:
-                    KAJONA_WEBPATH +
-                    '/xml.php?admin=1&module=dashboard&action=getCalendarEvents',
-                eventRender: function (event, el) {
-                    Tooltip.addTooltip(el, event.tooltip)
-                    if (event.icon) {
-                        el.find('span.fc-title').prepend(event.icon + ' ')
-                    }
-                },
-                loading: function (isLoading) {
-                    if (isLoading) {
-                        WorkingIndicator.start()
-                    } else {
-                        WorkingIndicator.stop()
-                    }
+    public static init () {
+        let calendarEl: HTMLElement = document.getElementById('dashboard-calendar')!
+        let calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin, listGridPlugin],
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,listWeek'
+            },
+            editable: false,
+            locales: [deLocale, enLocale],
+            locale: KAJONA_LANGUAGE,
+            eventLimit: true,
+            events: KAJONA_WEBPATH + '/xml.php?admin=1&module=dashboard&action=getCalendarEvents',
+            eventRender: function (info) {
+                Tooltip.addTooltip(info.el, info.event.extendedProps.tooltip)
+                if (info.event.extendedProps.icon) {
+                    info.el.innerHTML = info.el.innerHTML.replace('fc-title">', 'fc-title">' + info.event.extendedProps.icon + ' ')
                 }
-            })
-            $('.fc-button-group')
-                .removeClass()
-                .addClass('btn-group')
-            $('.fc-button')
-                .removeClass()
-                .addClass('btn btn-default')
+            },
+            loading: function (isLoading) {
+                if (isLoading) {
+                    WorkingIndicator.start()
+                } else {
+                    WorkingIndicator.stop()
+                }
+            }
         })
+
+        calendar.render()
+
+        $('.fc-button-group').removeClass().addClass('btn-group')
+        $('.fc-button').removeClass().addClass('btn btn-default')
     }
 
-    public static getICalendarURL() {
+    public static getICalendarURL () {
         Ajax.genericAjaxCall(
             'dashboard',
             'apiGetOrCreateICalUrl',
@@ -53,13 +56,9 @@ class DashboardCalendar {
                 if (data) {
                     var modalContent =
                         '<div class="input-group">' +
-                        '<input type="text" class="form-control" value="' +
-                        data.url +
-                        '">' +
+                        '<input type="text" class="form-control" value="' + data.url + '">' +
                         '<span class="input-group-btn">' +
-                        '<button class="btn btn-default copy-btn" type="button" title="" onclick="Util.copyTextToClipboard(\'' +
-                        data.url +
-                        '\')">' +
+                        '<button class="btn btn-default copy-btn" type="button" title="" onclick="Util.copyTextToClipboard(\'' + data.url + '\')">' +
                         "<i class='kj-icon fa fa-clipboard'>" +
                         '</button>' +
                         '</span>' +
@@ -67,21 +66,13 @@ class DashboardCalendar {
 
                     DialogHelper.showInfoModal('', modalContent)
 
-                    Lang.fetchSingleProperty(
-                        'system',
-                        'copy_to_clipboard',
-                        function (value: string) {
-                            $('.copy-btn').attr('title', value)
-                        }
-                    )
+                    Lang.fetchSingleProperty('system', 'copy_to_clipboard', function (value: string) {
+                        $('.copy-btn').attr('title', value)
+                    })
 
-                    Lang.fetchSingleProperty(
-                        'system',
-                        'copy_page_url',
-                        function (value: string) {
-                            $('#jsDialog_0_title').text(value)
-                        }
-                    )
+                    Lang.fetchSingleProperty('system', 'copy_page_url', function (value: string) {
+                        $('#jsDialog_0_title').text(value)
+                    })
                 }
             },
             null,
