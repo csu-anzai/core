ac2b5f3a890ee1316729b99f2f3e2f29cb51c17b
Dhafer <soprano1993@live.fr>
2019-08-30 17:50:31 +0200

    feat(systemn) : added totalElements as a property + emit event on totalElements click

--- module_system/scripts/components/Pagination/Pagination.vue
+++ module_system/scripts/components/Pagination/Pagination.vue
@@ -1,24 +1,24 @@
 <template>
-  <div v-if="langFetched" class="pager core-component-pager">
+  <div v-if="langFetched" class="pager">
     <ul class="pagination">
       <li v-if="current!==1" @click="previous()">
         <a>« {{$t('commons.commons_back')}}</a>
       </li>
       <li
-        v-for="(item, index) in total"
+        v-for="(item, index) in totalPages"
         :key="index"
         v-bind:class="{active : current === index+1}"
         @click="changePage(index+1)"
       >
         <a v-bind:class="{active : current === index+1}">{{index+1}}</a>
       </li>
-      <li @click="next()" v-if="current!==total">
+      <li @click="next()" v-if="current!==totalPages">
         <a>{{$t('commons.commons_continue')}} »</a>
       </li>
-      <li>
+      <li @click="onTotalElementsClick">
         <a>
           {{$t("system.pageview_total")}}
-          {{total}}
+          {{totalElements}}
         </a>
       </li>
     </ul>
